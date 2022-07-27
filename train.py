80e2699e5855024b4c2cde769e243f5e8691ff29
Thorben <tfinke95@gmail.com>
2022-07-27 14:04:05 +0200

    Added schedules, validation, data parameter

--- train.py
+++ train.py
@@ -15,10 +15,24 @@ import pandas as pd
 torch.multiprocessing.set_sharing_strategy('file_system')
 
 
-def get_linear_scheduler():
+def get_lin_scheduler():
     training_steps = args.num_epochs * len(train_loader)
     lr_fn = lambda step: 1.0 - ((1.0 - args.lr_decay) * (step / training_steps))
     scheduler = LambdaLR(opt, lr_lambda=lr_fn)
+    return scheduler
+
+
+def get_exp_scheduler(final_reduction=1e-2):
+    training_steps = args.num_epochs * len(train_loader)
+    lr_fn = lambda step: final_reduction ** (step / training_steps)
+    scheduler = LambdaLR(opt, lr_lambda=lr_fn)
+    return scheduler
+
+
+def get_cos_scheduler():
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+        opt, T_0=len(train_loader), T_mult=2
+    )
     return scheduler
 
 
@@ -38,7 +52,6 @@ def load_opt_states():
     opt.load_state_dict(state_dicts['opt_state_dict'])
     scheduler.load_state_dict(state_dicts['sched_state_dict'])
     scaler.load_state_dict(state_dicts['scaler_state_dict'])
-    return opt, scheduler, scaler
 
 
 def save_model(name):
@@ -53,14 +66,20 @@ def load_model(name):
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--model_dir", type=str, default='models/test', help="Model directory")
-    parser.add_argument("--data_path", type=str, default='Datasets/train_top.h5', help="Path to training data file")
+    parser.add_argument("--data_path", type=str, default='/hpcwork/bn227573/top_benchmark/train_qcd_30_bins.h5', help="Path to training data file")
 
     parser.add_argument("--seed", type=int, default=0, help="the random seed for torch and numpy")
     parser.add_argument("--logging_steps", type=int, default=10, help="Training steps between logging")
     parser.add_argument("--checkpoint_steps", type=int, default=5000, help="Training steps between saving checkpoints")
     parser.add_argument("--num_workers", type=int, default=4, help="Number of workers")
 
-    parser.add_argument("--num_epochs", type=int, default=100, help="Number of epochs")
+    parser.add_argument("--num_const", type=int, default=100, help="Number of constituents")
+    parser.add_argument("--num_events", type=int, default=10000, help="Number of events for training")
+    parser.add_argument("--num_bins", type=int, nargs=3, default=[41, 31, 31], help="Number of bins per feature")
+    parser.add_argument("--contin", action='store_true', help="Whether to continue training")
+    parser.add_argument("--global_step", type=int, default=0, help="Starting point of step counter")
+
+    parser.add_argument("--num_epochs", type=int, default=3, help="Number of epochs")
     parser.add_argument("--batch_size", type=int, default=100, help="Batch size")
     parser.add_argument("--lr", type=float, default=0.001, help="learning rate")
     parser.add_argument("--lr_decay", type=float, default=0.01, help="learning rate decay (linear)")
@@ -76,13 +95,18 @@ if __name__ == '__main__':
     np.random.seed(args.seed)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"Running on device: {device}")
 
     num_features = 3
     num_bins = (41, 41, 41)
 
     # load and preprocess data
     df = pd.read_hdf(args.data_path, 'discretized')
-    x, padding_mask, bins = preprocess_dataframe(df, num_features=num_features, num_bins=num_bins, to_tensor=True)
+    x, padding_mask, bins = preprocess_dataframe(df, num_features=num_features,
+                                num_bins=num_bins,
+                                to_tensor=True,
+                                num_const=args.num_const,
+                                num_events=args.num_events)
 
     train_dataset = TensorDataset(x, padding_mask, bins)
     train_loader = DataLoader(
@@ -91,6 +115,23 @@ if __name__ == '__main__':
         num_workers=args.num_workers,
         shuffle=True,
     )
+    print(f"Test set shape: {x.shape}")
+
+    df = pd.read_hdf(args.data_path.replace('train', 'test'), 'discretized')
+    x, padding_mask, bins = preprocess_dataframe(df, num_features=num_features,
+                                num_bins=num_bins,
+                                to_tensor=True,
+                                num_const=args.num_const,
+                                num_events=10000)
+
+    val_dataset = TensorDataset(x, padding_mask, bins)
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        shuffle=False,
+    )
+    print(f"Val set shape: {x.shape}")
 
     # construct model
     model = JetTransformer(
@@ -106,15 +147,16 @@ if __name__ == '__main__':
 
     # construct optimizer and auto-caster
     opt = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-    scheduler = get_linear_scheduler()
+    scheduler = get_exp_scheduler()
     scaler = torch.cuda.amp.GradScaler()
 
     logger = SummaryWriter(args.model_dir)
-    global_step = 0
+    global_step = args.global_step
     loss_list = []
     perplexity_list = []
     for epoch in range(args.num_epochs):
         model.train()
+        print(f'LR:{scheduler.get_last_lr()}')
 
         for x, padding_mask, true_bin in tqdm(train_loader, total=len(train_loader), desc=f'Training Epoch {epoch + 1}'):
             opt.zero_grad()
@@ -146,6 +188,26 @@ if __name__ == '__main__':
             #    save_model(f'checkpoint_{global_step}')
 
             global_step += 1
+
+        model.eval()
+        with torch.no_grad():
+            val_loss = []
+            val_perplexity = []
+            for x, padding_mask, true_bin in tqdm(val_loader, total=len(val_loader), desc=f'Validation Epoch {epoch + 1}'):
+                x = x.to(device)
+                padding_mask = padding_mask.to(device)
+                true_bin = true_bin.to(device)
+
+                logits = model(x, padding_mask,)
+                loss = model.loss(logits, true_bin)
+                perplexity = model.probability(logits, padding_mask, true_bin, perplexity=True, logarithmic=False)
+                val_loss.append(loss.cpu().detach().numpy())
+                val_perplexity.append(perplexity.mean().cpu().detach().numpy())
+
+            logger.add_scalar('Val/Loss', np.mean(val_loss), global_step)
+            logger.add_scalar('Val/Perplexity', np.mean(val_perplexity), global_step)
+
+        print(f'LR:{scheduler.get_last_lr()}')
 
         save_model('last')
         #save_opt_states(model.model_dir)
