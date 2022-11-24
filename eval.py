529368f3fe8c50cc9f87562ebf2539356a7cef89
Thorben Finke <finke@physik.rwth-aachen.de>
2022-11-24 16:23:22 +0100

    Using my eval

--- eval.py
+++ eval.py
@@ -9,94 +9,148 @@ from argparse import ArgumentParser
 from tqdm import tqdm
 import os
 import pandas as pd
-from glob import glob
+import matplotlib.pyplot as plt
 
 from sklearn.metrics import roc_auc_score, roc_curve
 
 torch.multiprocessing.set_sharing_strategy('file_system')
 
-
 def load_model(model_dir, name):
     model = torch.load(os.path.join(model_dir, f'model_{name}.pt'))
     return model
-
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--model_dir", type=str, default='models/test', help="Model directory")
     parser.add_argument("--model_name", type=str, default='last', help="Checkpoint name")
-    parser.add_argument("--data_path", type=str, default='Datasets/', help="Path to training data file")
+    parser.add_argument("--data_path", type=str, default='/hpcwork/bn227573/top_benchmark/', help="Path to training data file")
     parser.add_argument("--data_split", type=str, default='test', help="Split to evaluate")
     parser.add_argument("--num_workers", type=int, default=1, help="Number of workers")
+    parser.add_argument("--num_const", type=int, default=100, help="Number of constituents")
+    parser.add_argument("--num_events", type=int, default=200000, help="Number of events")
     parser.add_argument("--batch_size", type=int, default=200, help="Batch size")
+    parser.add_argument("--bkg", type=str, default='qcd', choices=['qcd', 'top'])
+    parser.add_argument("--reverse", action="store_true", help="Reverse the pt ordering")
+    parser.add_argument("--limit_const", action="store_true", help="Only use jets with at least num_const constituents")
+    parser.add_argument("--kind", type=str, default='perp', choices=['perp', 'log'], help='Kind of anomaly score')
     args = parser.parse_args()
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f'Running on device {device}')
+
+    if args.kind == 'perp':
+        perp, log = True, False
+    elif args.kind == 'log':
+        perp, log = False, True
 
     num_features = 3
-    num_bins = (41, 41, 41)
-    classes = ['top', 'qcd']
+    num_bins = (41, 31, 31)
 
-    # load model
-    models = {}
-    for c in classes:
-        # model_dir = os.path.join(args.model_dir, c)
-        model_dir = args.model_dir + c
-        print(model_dir)
-        model = load_model(model_dir, args.model_name)
-        model.eval()
-        model.to(device)
-        models[c] = model
+    print(f'Loading model from {args.model_dir+args.bkg}')
+    model = load_model(args.model_dir+args.bkg, 'last')
+    model.eval()
+    model.to(device)
 
-    perplexity_list = []
-    y_list = []
-    for i, data_class in enumerate(classes):
-        data_path = os.path.join(args.data_path, f'{args.data_split}_{data_class}.h5')
-        df = pd.read_hdf(data_path, 'discretized')
-        x, padding_mask, bins = preprocess_dataframe(df, num_features=num_features, num_bins=num_bins, num_const=40, num_events=1000, to_tensor=True)
+    scores = []
+    labels = []
+    nparts = []
+    losses = []
+    probs_min = []
+    probs_min_idx = []
+    probs_max = []
+    probs_max_idx = []
+    for c in ['qcd', 'top',]:
+        print(c)
+        tmp_losses = []
+        data_path = os.path.join(args.data_path, f'test_{c}_30_bins.h5')
+        df = pd.read_hdf(data_path, 'discretized', stop=args.num_events)
+        x, padding_mask, bins = preprocess_dataframe(df,
+            num_features=num_features,
+            num_bins=num_bins,
+            num_const=args.num_const,
+            to_tensor=True,
+            reverse=args.reverse,
+            limit_nconst=args.limit_const
+            )
+        labels.append(int(c!=args.bkg) * np.ones(len(x)))
 
         test_dataset = TensorDataset(x, padding_mask, bins)
         test_loader = DataLoader(
             test_dataset,
             batch_size=args.batch_size,
             num_workers=args.num_workers,
-        )
+            )
 
-        for x, padding_mask, true_bin in tqdm(test_loader, total=len(test_loader), desc=f'Evaluating {data_path}'):
+        for x, padding_mask, true_bin in tqdm(test_loader,
+                total=len(test_loader),
+                desc=f'Evaluating {data_path}'
+                ):
+            nparts.append(padding_mask.sum(-1))
             x = x.to(device)
             padding_mask = padding_mask.to(device)
             true_bin = true_bin.to(device)
 
-            batch_perplexity = []
-            for model_class in classes:
-                model = models[model_class]
-                with torch.no_grad():
-                    with torch.cuda.amp.autocast():
-                        logits = model(x, padding_mask)
-                        perplexity = model.probability(logits, padding_mask, true_bin, perplexity=False, logarithmic=True)
-                batch_perplexity.append(perplexity.cpu().detach().numpy().reshape(-1, 1))
+            with torch.no_grad():
+                with torch.cuda.amp.autocast():
+                    logits = model(x, padding_mask)
+                    probs = torch.nn.Softmax(dim=-1)(logits)
+                    tmp = probs.min(-1)
+                    prob_min = tmp.values.cpu().detach().numpy()
+                    prob_min_idx = tmp.indices.cpu().detach().numpy()
 
-            batch_perplexity = np.hstack(batch_perplexity)
-            batch_y = np.int64([i]*x.shape[0])
-            perplexity_list.append(batch_perplexity)
-            y_list.append(batch_y)
+                    tmp = probs.max(-1)
+                    prob_max = tmp.values.cpu().detach().numpy()
+                    prob_max_idx = tmp.indices.cpu().detach().numpy()
 
-    perplexity = np.vstack(perplexity_list)
-    score_direct = -perplexity[:,1]
-    score_reverse = -perplexity[:,0]
-    y = np.concatenate(y_list, axis=0)
-    auc_direct = roc_auc_score(y_true=np.abs(y-1), y_score=score_direct)
-    auc_reverse = roc_auc_score(y_true=y, y_score=score_reverse)
-    print(auc_direct)
-    print(auc_reverse)
-    exit()
+                    loss = model.loss_pC(logits, true_bin)
+                    perplexity = model.probability(logits,
+                        padding_mask,
+                        true_bin,
+                        perplexity=perp,
+                        logarithmic=log
+                        )
+                    probs_min.append(prob_min)
+                    probs_max.append(prob_max)
+                    probs_min_idx.append(prob_min_idx)
+                    probs_max_idx.append(prob_max_idx)
+                    tmp_losses.append(loss.reshape(len(x), -1).cpu().detach().numpy())
+                    scores.append(perplexity.cpu().detach().numpy())
 
 
+        tmp_losses = np.concatenate(tmp_losses, axis=0)
+        tmp_losses[bins[:, 1:] == -100] = np.nan
+        losses.append(tmp_losses,)
 
+    labels = np.concatenate(labels, axis=0)
+    scores = np.concatenate(scores, axis=0)
+    losses = np.concatenate(losses, axis=0)
+    probs_min = np.concatenate(probs_min, axis=0)
+    probs_min_idx = np.concatenate(probs_min_idx, axis=0)
+    probs_max = np.concatenate(probs_max, axis=0)
+    probs_max_idx = np.concatenate(probs_max_idx, axis=0)
 
-    score = perplexity[:, 1] / (2 * perplexity.sum(axis=1))
-    acc = np.float32(perplexity.argmax(axis=1) == y).mean()
-    auc = roc_auc_score(y_true=y, y_score=score)
-    fpr, tpr, _ = roc_curve(y_true=y, y_score=score)
-    # np.savez('tmp.npz', fpr=fpr, tpr=tpr)
-    print(f'Accuracy : {acc}, AUC: {auc}')
+    print(probs_min.shape)
+    print(probs_min_idx.shape)
+
+    probs = np.stack((probs_min, probs_max), axis=-1)
+    probs_idx = np.stack((probs_min_idx, probs_max), axis=-1)
+    print(probs.shape)
+    print(probs_idx.shape)
+
+    print(f'Loss {np.shape(losses)}')
+    print(f'Label {np.shape(labels)}')
+    print(f'Scores {np.shape(scores)}')
+    print(f'Stats {np.shape(probs)}')
+
+    print(f'Nparts {np.shape(nparts)}')
+
+    np.savez(os.path.join(args.model_dir+args.bkg, f"predictions_{args.kind}{'_fixed' if args.limit_const else ''}.npz"),
+        labels=labels,
+        scores=scores,
+        nparts=nparts,
+        losses=losses,
+        probs=probs,
+        probs_idx=probs_idx,
+        )
+    auc = roc_auc_score(y_true=labels, y_score=-scores)
+    print(auc)
