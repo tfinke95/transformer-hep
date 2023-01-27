Minor fixes, masking value

model.py
@@ -267,7 +267,7 @@ class JetTransformer(Module):
 
                 # Remove low probs
                 if not trunc is None:
-                    preds = torch.where(preds < trunc, 0, preds)
+                    preds = torch.where(preds < trunc, torch.zeros(1, device=device), preds)
                 preds = preds / torch.sum(preds, -1, keepdim=True)
 
                 idx = select_idx()

particlenet/data_loader.py
@@ -3,9 +3,9 @@ import sys, json, os
 import pandas as pd
 
 
-def transform_momenta(momenta, features="last"):
+def transform_momenta(momenta, masking_value=0):
     pts = momenta[:, :, 0]
-    mask = pts != -100
+    mask = pts != masking_value
     etas = momenta[:, :, 1]
     phis = momenta[:, :, 2]
 
@@ -23,7 +23,7 @@ def transform_momenta(momenta, features="last"):
         -1,
     )
     for i in range(newVec.shape[-1]):
-        newVec[~mask, i] = -100
+        newVec[~mask, i] = masking_value
     return newVec
 
 
@@ -52,10 +52,10 @@ def get_config(test=False):
     return config
 
 
-def make_continues(jets, noise=False):
-    pt_bins = np.load("../preprocessing_bins/pt_bins_30_bins.npy")
-    eta_bins = np.load("../preprocessing_bins/eta_bins_30_bins.npy")
-    phi_bins = np.load("../preprocessing_bins/phi_bins_30_bins.npy")
+def make_continues(jets, noise=False, masking_value=0):
+    pt_bins = np.load("../preprocessing_bins/pt_bins_pt40_eta30_phi30_lower001.npy")
+    eta_bins = np.load("../preprocessing_bins/eta_bins_pt40_eta30_phi30_lower001.npy")
+    phi_bins = np.load("../preprocessing_bins/phi_bins_pt40_eta30_phi30_lower001.npy")
 
     pt_disc = jets[:, :, 0]
     print(pt_disc.max(), pt_bins.shape)
@@ -81,27 +81,28 @@ def make_continues(jets, noise=False):
         phi_con = phi_disc * (phi_bins[1] - phi_bins[0]) + phi_bins[0]
 
     continues_jets = np.stack((np.exp(pt_con), eta_con, phi_con), -1)
-    continues_jets[mask] = -100
+    continues_jets[mask] = masking_value
 
     return continues_jets
 
 
-def load_data(params, test=False, plot_dists=None):
+def load_data(params, test=False, plot_dists=None, masking_value=0):
     def load_file(file, key=None):
         # Load  sample file
         if file.endswith("npz"):
             if test:
-                file = file.replace("50", "100_test")
-            print(file)
+                file = file.replace("train_", "test_")
+            print(f"\nChanged file to {file}")
             dat = np.load(file)["jets"][: params["n_jets"], : params["n_const"]]
         # Load data file
         elif file.endswith("h5"):
             if test:
+                file = file.replace("val", "test")
+                print(f"\nChanged file to {file}")
                 dat = pd.read_hdf(
                     file,
                     key=key,
-                    start=100000,
-                    stop=100000 + params["n_jets"],
+                    stop=params["n_jets"],
                 )
             else:
                 dat = pd.read_hdf(file, key=key, stop=params["n_jets"])
@@ -115,28 +116,28 @@ def load_data(params, test=False, plot_dists=None):
         return dat
 
     print(params["bg_file"])
-    print(params["sig_files"])
+    print(params["sig_file"])
 
     bg = load_file(params["bg_file"], key=params["bg_key"])
     if params["bg_key"] == "discretized":
-        print("BG made continuous")
-        bg = make_continues(bg, params["bg_noise"])
+        print(f"BG made continuous, with noise {params['bg_noise']}\n")
+        bg = make_continues(bg, params["bg_noise"], masking_value=masking_value)
     else:
-        bg[bg[:, :, 0] == 0] = -100
+        bg[bg[:, :, 0] == 0] = masking_value
 
-    sig = load_file(params["sig_files"][0], key=params["sig_key"])
+    sig = load_file(params["sig_file"], key=params["sig_key"])
     if params["sig_key"] == "discretized":
-        print("Sig made continuous")
-        sig = make_continues(sig, params["sig_noise"])
+        print(f"Sig made continuous, with noise {params['bg_noise']}\n")
+        sig = make_continues(sig, params["sig_noise"], masking_value=masking_value)
     else:
-        sig[sig[:, :, 0] == 0] = -100
-    print(bg.shape, sig.shape)
+        sig[sig[:, :, 0] == 0] = masking_value
+    print(f"Bg shape {bg.shape}, Sig shape {sig.shape}\n")
 
     data = np.append(bg, sig, 0)
     labels = np.append(np.zeros(len(bg)), np.ones(len(sig)))
     shuffle = np.random.permutation(len(data))
 
-    data = transform_momenta(data)
+    data = transform_momenta(data, masking_value=masking_value)
 
     if not plot_dists is None:
         import matplotlib.pyplot as plt
@@ -146,10 +147,10 @@ def load_data(params, test=False, plot_dists=None):
             features, 1, constrained_layout=True, figsize=(features * 3, 10)
         )
         for i in range(data.shape[-1]):
-            range_min = data[:, :, i][data[:, :, i] != -100].min()
+            range_min = data[:, :, i][data[:, :, i] != masking_value].min()
             range_max = data[:, :, i].max()
             axes[i].hist(
-                data[labels == 0, :, i][data[labels == 0, :, 0] != -100].flatten(),
+                data[labels == 0, :, i][data[labels == 0, :, 0] != masking_value].flatten(),
                 bins=300,
                 range=[range_min, range_max],
                 histtype="step",
@@ -157,7 +158,7 @@ def load_data(params, test=False, plot_dists=None):
                 label="Background",
             )
             axes[i].hist(
-                data[labels == 1, :, i][data[labels == 1, :, 0] != -100].flatten(),
+                data[labels == 1, :, i][data[labels == 1, :, 0] != masking_value].flatten(),
                 bins=300,
                 range=[range_min, range_max],
                 histtype="step",

particlenet/evaluate.py
@@ -17,7 +17,7 @@ NETWOKPARAMS = {
     "static": False,
 }
 TEST = True
-
+MASKING_VALUE = 0
 
 def plot_trainHistory(folder):
     fig, ax = plt.subplots(constrained_layout=True)
@@ -37,7 +37,7 @@ def plot_trainHistory(folder):
     ax.set_xlabel("Epoch")
     ax.set_ylabel("Loss")
     ax.set_xticks(np.arange(0, max_ep + 1, 5))
-    fig.savefig(os.path.join(folder, "training.pdf"))
+    fig.savefig(os.path.join(folder, "training.png"))
 
 
 def plot_roc(predictions, labels, folder, plot=False):
@@ -68,7 +68,7 @@ def plot_roc(predictions, labels, folder, plot=False):
         ax.set_yscale("log")
         ax.set_ylim(1, 6e3)
         ax.grid(which="both")
-        fig.savefig(os.path.join(folder, f"roc_{'test' if TEST else 'train'}.pdf"))
+        fig.savefig(os.path.join(folder, f"roc_{'test' if TEST else 'train'}.png"))
         plt.close(fig)
 
 
@@ -77,7 +77,7 @@ def plot_high_low_preds(data, preds, folder):
     low = np.quantile(preds[:, 1], 0.1)
     fig, axes = plt.subplots(5, constrained_layout=True)
     for i in range(5):
-        range_min = data[1][:, :, i][data[1][:, :, i] != -100].min()
+        range_min = data[1][:, :, i][data[1][:, :, i] != MASKING_VALUE].min()
         range_max = data[1][:, :, i].max()
         axes[i].hist(
             (data[1][preds[:, 1] > high, :, i] * data[2][preds[:, 1] > high]).min(1),
@@ -112,7 +112,7 @@ def plot_high_low_preds(data, preds, folder):
             label="low",
         )
     axes[0].legend()
-    fig.savefig(os.path.join(folder, f"high_and_low.pdf"))
+    fig.savefig(os.path.join(folder, f"high_and_low.png"))
 
     plt.show()
     plt.close(fig)
@@ -133,17 +133,21 @@ def check_weights(model, folder, data, load=True):
 def main():
     model = GraphNet(**NETWOKPARAMS)
     config = get_config(test=True)
-    config["data"]["n_jets"] = 10000
     data, labels = load_data(
         config["data"],
         test=TEST,
         plot_dists=os.path.join(config["logging"]["logfolder"], "dists.png"),
+        masking_value=MASKING_VALUE,
     )
-    print(f"{data.shape=} {labels.shape=}")
+    print(f"\nData shape {data.shape} Labels shape {labels.shape}")
+    print(f"First labels {labels[:15]}\n")
+
     model([data[:2, :, :2], data[:2]])
     model = check_weights(model, config["logging"]["logfolder"], data=data)
     if config["mask"]:
-        data = [data[:, :, :2], data, data[:, :, 2] != -100]
+        data = [data[:, :, :2], data, data[:, :, 2] != MASKING_VALUE]
+        print(data[0][0])
+        print(data[-1][0])
     else:
         data = [data[:, :, :2], data]
     preds = model.predict(data, batch_size=1024, verbose=1)

particlenet/train.py
@@ -11,6 +11,7 @@ tfk = tf.keras
 gpu = tf.config.list_physical_devices("GPU")
 assert len(gpu) > 0, f"No GPU found, abord"
 
+MASKING_VALUE = 0
 
 def train_model(model, data, labels, train_params, logfolder, mask):
     optimizer = tfk.optimizers.Adam(learning_rate=0.001)
@@ -20,7 +21,7 @@ def train_model(model, data, labels, train_params, logfolder, mask):
         metrics=[tfk.metrics.AUC(), "acc"],
     )
     if mask:
-        mask = data[:, :, 2] != 0
+        mask = data[:, :, 2] != MASKING_VALUE
         train_data = [data[:, :, :2], data, mask]
     else:
         train_data = [
@@ -42,7 +43,7 @@ def train_model(model, data, labels, train_params, logfolder, mask):
         train_data,
         tfk.utils.to_categorical(labels, 2),
         callbacks=[
-            tfk.callbacks.ReduceLROnPlateau(monitor="loss", patience=8, verbose=1),
+            tfk.callbacks.ReduceLROnPlateau(monitor="val_loss", patience=8, verbose=1),
             tfk.callbacks.EarlyStopping(
                 monitor="val_loss", patience=12, verbose=1, restore_best_weights=True
             ),
@@ -74,7 +75,11 @@ def main():
     config = check_activation(get_config())
     model = GraphNet(**config["graphnet"])
     start = time.time()
-    data, true_label = load_data(config["data"], plot_dists="tmp.png")
+    data, true_label = load_data(
+        config["data"],
+        plot_dists=os.path.join(config["logging"]["logfolder"], "distributions.png"),
+        masking_value=MASKING_VALUE)
+    print(f"First labels {true_label[:15]}\n")
 
     train_model(
         model=model,
@@ -86,7 +91,7 @@ def main():
     )
     model.save_weights(os.path.join(config["logging"]["logfolder"], "model_weights.h5"))
     if config["mask"]:
-        mask = data[:, :, 2] != 0
+        mask = data[:, :, 2] != MASKING_VALUE
         test_data = [data[:, :, :2], data, mask]
     else:
         test_data = [data[:, :, :2], data]

plotter.py
@@ -2,6 +2,8 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import roc_auc_score, roc_curve
+import pandas as pd
+from tqdm import tqdm
 
 size = 12
 FIGSIZE = (4,8/3)
@@ -242,13 +244,44 @@ def plot_max_index(dir):
     fig.savefig('idx.png')
 
 
+def plot_bin_distribution():
+    bg_file = "/hpcwork/rwth0934/top_benchmark/discretized/train_qcd_pt40_eta30_phi30.h5"
+    from preprocess import preprocess_dataframe
+
+    bg_data = pd.read_hdf(bg_file, key="discretized", stop=None)
+
+    bg_data, mask, bg_bins = preprocess_dataframe(
+        bg_data, 3, (41, 31, 31), 50, False, False,False, False, False,
+    )
+
+    bins = np.zeros(39402)
+    for i in tqdm(bg_bins[mask].flatten()):
+        bins[i] += 1
+    bins /= bins.sum()
+    print(bins[bins!=0].min())
+    print((bins==0).sum())
+    print((bins!=0).sum())
+
+    fig, axes = plt.subplots(ncols=3, figsize=(15, 8), sharey=True, constrained_layout=True)
+
+    max_bin = np.argmax(bins)
+    axes[0].plot(bins)
+    axes[1].plot(bins)
+    axes[2].plot(bins)
+    axes[1].set_xlim(max_bin - (41 * 31) / 2, max_bin + 41 * 31 * 1.5)
+    axes[2].set_xlim(max_bin - 41 * 3.5, max_bin + 41 * 3.5)
+    axes[0].set_yscale("log")
+    axes[0].set_xlabel("bin number")
+    fig.savefig("figures/bins_dist.png")
+
 
 if __name__ == '__main__':
-    dirname = '1part/20fixed'
+    plot_bin_distribution()
+    # dirname = '1part/20fixed'
     # dirname = 'N600k_E100_nC100_nBin30_LRcos_reverse'
     # plot_scores(dirname)
-    plot_rocs(dirname, sic=False)
-    plot_rocs(dirname, sic=True)
+    # plot_rocs(dirname, sic=False)
+    # plot_rocs(dirname, sic=True)
     # plot_loss_pC(dirname)
     # plot_probs(dirname)
     # plot_max_index(dirname)

sample_jets.py
@@ -20,6 +20,7 @@ parser.add_argument("--num_samples", type=int, default=1000)
 parser.add_argument("--batchsize", type=int, default=100)
 parser.add_argument("--num_const", type=int, default=50)
 parser.add_argument("--seed", type=int, default=0)
+parser.add_argument("--trunc", type=float, default=None)
 
 args = parser.parse_args()
 
@@ -45,6 +46,7 @@ for i in tqdm(range(n_batches), total=n_batches, desc="Sampling batch"):
         starts=torch.zeros((args.batchsize, 3), device=device),
         device=device,
         len_seq=args.num_const + 1,
+        trunc=args.trunc,
     )
     jets.append(_jets.cpu().numpy())
     bins.append(_bins.cpu().numpy())
@@ -61,11 +63,11 @@ if rest != 0:
 
 jets = np.concatenate(jets, 0)
 bins = np.concatenate(bins, 0)
-print(jets.dtype)
 
 np.savez(
     os.path.join(args.model_dir, f"samples_{args.savetag}"),
     jets=jets[:, 1:],
     bins=bins[:, 1:],
 )
-print(int(time.time() - start))
+print(f"Time needed {(time.time() - start) / float(args.num_samples)} seconds per jet")
+print(f"\t{int(time.time() - start)} seconds in total")

test_samples.py
@@ -92,15 +92,15 @@ def plot_data_dist():
         axes[i].hist(
             bg[:, :, i][bg[:, :, 0] != 0].flatten(),
             histtype="step",
-            bins=nBins,
-            density=False,
+            bins=np.linspace(-0.5, nBins + 0.5, nBins + 2),
+            density=True,
             label="Background",
         )
         axes[i].hist(
             sig[:, :, i][sig[:, :, 0] != 0].flatten(),
             histtype="step",
-            bins=nBins,
-            density=False,
+            bins=np.linspace(-0.5, nBins + 0.5, nBins + 2),
+            density=True,
             label="Signal",
         )
         if i == 0:
@@ -220,7 +220,6 @@ plot_data_dist()
 data = jets_to_images(data)
 
 # %%
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 classi = CNNclass()
@@ -293,7 +292,6 @@ ax.legend()
 fig.savefig(os.path.join(args.save_dir, "roc.png"))
 
 # %%
-
 fig, ax = plt.subplots(constrained_layout=True)
 ax.hist([preds[labels == 0], preds[labels == 1]], bins=100, histtype="step")
 ax.set_yscale("log")