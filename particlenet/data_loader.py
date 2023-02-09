c16c567e83bf51dd5e45496974642ad19016593b
Thorben <tfinke95@gmail.com>
2023-02-09 16:08:03 +0100

    Updated masking in dataloader for particlenet

--- particlenet/data_loader.py
+++ particlenet/data_loader.py
@@ -3,27 +3,34 @@ import sys, json, os
 import pandas as pd
 
 
-def transform_momenta(momenta, masking_value=0):
+def transform_momenta(momenta, mask):
     pts = momenta[:, :, 0]
-    mask = pts != masking_value
     etas = momenta[:, :, 1]
     phis = momenta[:, :, 2]
 
     drs = np.sqrt(np.sum(np.square(momenta[:, :, 1:3]), -1))
 
-    pxs = np.cos(phis) * pts * mask
-    pys = np.sin(phis) * pts * mask
+    pxs = np.cos(phis) * pts
+    pys = np.sin(phis) * pts
+    pxs[~mask] = 0
+    pys[~mask] = 0
+
     pxj = np.sum(pxs, -1)
     pyj = np.sum(pys, 1)
-
     ptj = np.sqrt(pxj**2 + pyj**2)
 
+    etas[~mask] = 0
+    phis[~mask] = 0
+    logpts = np.log(pts)
+    logpts[~mask] = 0
+    logpt_ptj = np.log(pts / ptj.reshape(-1, 1))
+    logpt_ptj[~mask] = 0
+    drs[~mask] = 0
+
     newVec = np.stack(
-        [etas, phis, np.log(pts), np.log(pts / ptj.reshape(-1, 1)), drs],
+        [etas, phis, logpts, logpt_ptj, drs],
         -1,
     )
-    for i in range(newVec.shape[-1]):
-        newVec[~mask, i] = masking_value
     return newVec
 
 
@@ -52,18 +59,14 @@ def get_config(test=False):
     return config
 
 
-def make_continues(jets, noise=False, masking_value=0):
+def make_continues(jets, mask, noise=False):
     pt_bins = np.load("../preprocessing_bins/pt_bins_pt40_eta30_phi30_lower001.npy")
     eta_bins = np.load("../preprocessing_bins/eta_bins_pt40_eta30_phi30_lower001.npy")
     phi_bins = np.load("../preprocessing_bins/phi_bins_pt40_eta30_phi30_lower001.npy")
 
     pt_disc = jets[:, :, 0]
-    print(pt_disc.max(), pt_bins.shape)
-    mask = pt_disc == 0
     eta_disc = jets[:, :, 1]
-    print(eta_disc.max(), eta_bins.shape)
     phi_disc = jets[:, :, 2]
-    print(phi_disc.max(), phi_bins.shape)
 
     if noise:
         pt_con = (pt_disc - np.random.uniform(0.0, 1.0, size=pt_disc.shape)) * (
@@ -76,14 +79,17 @@ def make_continues(jets, noise=False, masking_value=0):
             phi_bins[1] - phi_bins[0]
         ) + phi_bins[0]
     else:
-        pt_con = pt_disc * (pt_bins[1] - pt_bins[0]) + pt_bins[0]
-        eta_con = eta_disc * (eta_bins[1] - eta_bins[0]) + eta_bins[0]
-        phi_con = phi_disc * (phi_bins[1] - phi_bins[0]) + phi_bins[0]
+        pt_con = (pt_disc - 0.5) * (pt_bins[1] - pt_bins[0]) + pt_bins[0]
+        eta_con = (eta_disc - 0.5) * (eta_bins[1] - eta_bins[0]) + eta_bins[0]
+        phi_con = (phi_disc - 0.5) * (phi_bins[1] - phi_bins[0]) + phi_bins[0]
 
-    continues_jets = np.stack((np.exp(pt_con), eta_con, phi_con), -1)
-    continues_jets[mask] = masking_value
 
-    return continues_jets
+    pt_con = np.exp(pt_con)
+    pt_con[~mask] = 0.0
+    eta_con[~mask] = 0.0
+    phi_con[~mask] = 0.0
+
+    return np.stack((pt_con, eta_con, phi_con), -1)
 
 
 def load_data(params, test=False, plot_dists=None, masking_value=0):
@@ -94,6 +100,7 @@ def load_data(params, test=False, plot_dists=None, masking_value=0):
                 file = file.replace("train_", "test_")
             print(f"\nChanged file to {file}")
             dat = np.load(file)["jets"][: params["n_jets"], : params["n_const"]]
+            mask = dat.sum(-1) != 0
         # Load data file
         elif file.endswith("h5"):
             if test:
@@ -109,36 +116,40 @@ def load_data(params, test=False, plot_dists=None, masking_value=0):
                 dat = pd.read_hdf(file, key=key, stop=params["n_jets"])
             dat = dat.to_numpy()[:, : params["n_const"] * 3]
             dat = dat.reshape(dat.shape[0], -1, 3)
+            mask = dat[:, :, 0] != -1
         else:
             assert False, "Filetype for bg not supported"
         # Delete empty jest (can occur in samplings)
+        mask = np.delete(mask, np.where(dat[:, 0, 0] == 0)[0], axis=0)
         dat = np.delete(dat, np.where(dat[:, 0, 0] == 0)[0], axis=0)
         dat[dat == -1] = 0
-        return dat
+        return dat, mask
 
     print(params["bg_file"])
     print(params["sig_file"])
 
-    bg = load_file(params["bg_file"], key=params["bg_key"])
+    bg, bg_mask = load_file(params["bg_file"], key=params["bg_key"])
+    sig, sig_mask = load_file(params["sig_file"], key=params["sig_key"])
+
     if params["bg_key"] == "discretized":
         print(f"BG made continuous, with noise {params['bg_noise']}\n")
-        bg = make_continues(bg, params["bg_noise"], masking_value=masking_value)
+        bg = make_continues(bg, mask=bg_mask, noise=params["bg_noise"],)
     else:
-        bg[bg[:, :, 0] == 0] = masking_value
+        bg[~bg_mask] = 0
 
-    sig = load_file(params["sig_file"], key=params["sig_key"])
     if params["sig_key"] == "discretized":
         print(f"Sig made continuous, with noise {params['bg_noise']}\n")
-        sig = make_continues(sig, params["sig_noise"], masking_value=masking_value)
+        sig = make_continues(sig, mask=sig_mask, noise=params["sig_noise"])
     else:
-        sig[sig[:, :, 0] == 0] = masking_value
+        sig[~sig_mask] = 0
+
     print(f"Bg shape {bg.shape}, Sig shape {sig.shape}\n")
 
+    bg = transform_momenta(bg, mask=bg_mask)
+    sig = transform_momenta(sig, mask=sig_mask)
     data = np.append(bg, sig, 0)
     labels = np.append(np.zeros(len(bg)), np.ones(len(sig)))
     shuffle = np.random.permutation(len(data))
-
-    data = transform_momenta(data, masking_value=masking_value)
 
     if not plot_dists is None:
         import matplotlib.pyplot as plt
@@ -148,10 +159,10 @@ def load_data(params, test=False, plot_dists=None, masking_value=0):
             features, 1, constrained_layout=True, figsize=(features * 3, 10)
         )
         for i in range(data.shape[-1]):
-            range_min = data[:, :, i][data[:, :, i] != masking_value].min()
+            range_min = data[:, :, i].min()
             range_max = data[:, :, i].max()
             axes[i].hist(
-                data[labels == 0, :, i][data[labels == 0, :, 0] != masking_value].flatten(),
+                bg[bg_mask, i],
                 bins=300,
                 range=[range_min, range_max],
                 histtype="step",
@@ -159,7 +170,7 @@ def load_data(params, test=False, plot_dists=None, masking_value=0):
                 label="Background",
             )
             axes[i].hist(
-                data[labels == 1, :, i][data[labels == 1, :, 0] != masking_value].flatten(),
+                sig[sig_mask, i],
                 bins=300,
                 range=[range_min, range_max],
                 histtype="step",
