5a5275667662934a87e0530dde8f80548d9638fe
HumbertosMac <you@example.com>
2024-04-24 19:41:06 +0200

    mother scan train

--- mother_train_scan.py
+++ mother_train_scan.py
@@ -129,10 +129,10 @@ def random_string():
 #list_of_jets=['TTBar','ZJetsToNuNu','HToBB','HToCC','HToGG','HToWW2Q1L','HToWW4Q','TTBarLep','WToQ','ZToQQ']
 list_of_jets=['TTBar']
 num_const_list=[50]
-num_epochs_list=[3]
+num_epochs_list=[50]
 lr_list=[.001,.0005]
 lr_decay_list=[.000001]
-num_events_list=[600]
+num_events_list=[600000]
 dropout_list=[0.1]
 num_heads_list=[4]
 num_layers_list=[8]
@@ -142,7 +142,7 @@ hidden_dim_list=[256]
 batch_size_list=[100]
 num_events_val=600
 ###Sampling parameters
-num_samples_test_list=[200]
+num_samples_test_list=[200000]
 train_batch_size=100
 num_const_test=100
 trunc_test_list=[5000]
@@ -154,7 +154,7 @@ bins_path_prefix='preprocessing_bins/'
 for jet in list_of_jets:
 
     mother_dir='/net/data_t2k/transformers-hep/JetClass/'+jet+'_models/'
-    tag_oftrain=jet+'_run_testscan_600k'
+    tag_oftrain=jet+'_run_scan_600k'
     data_path='/net/data_t2k/transformers-hep/JetClass/discretized/'+jet+'_train___10M_'+jet+'.h5'
     model_path=mother_dir+'/'+tag_oftrain
     log_dir='/net/data_t2k/transformers-hep/JetClass/'+jet+'_models/'+tag_oftrain
@@ -195,7 +195,7 @@ for jet in list_of_jets:
                                                 
                                                     name_sufix=random_string()
                                                 
-                                                    os.system('python train_2.py --data_path '+str(data_path)+' --model_path '+str(model_path)+' --log_dir '+str(log_dir)+'  --output '+str(output)+' --num_const '+str(num_const)+' --num_epochs '+str(num_epochs)+'  --lr '+str(lr)+' --lr_decay '+str(lr_decay)+' --batch_size '+str(batch_size)+' --num_events '+str(num_events)+' --dropout '+str(dropout)+' --num_heads '+str(num_heads)+' --num_layers '+str(num_layers)+' --num_bins '+str(num_bins)+' --weight_decay '+str(weight_decay)+' --hidden_dim '+str(hidden_dim)+' --end_token --start_token '+' --name_sufix '+str(name_sufix)+' --num_events_val '+str(num_events_val)+' --checkpoint_steps 1200000')
+                                                    os.system('python train_0.py --data_path '+str(data_path)+' --model_path '+str(model_path)+' --log_dir '+str(log_dir)+'  --output '+str(output)+' --num_const '+str(num_const)+' --num_epochs '+str(num_epochs)+'  --lr '+str(lr)+' --lr_decay '+str(lr_decay)+' --batch_size '+str(batch_size)+' --num_events '+str(num_events)+' --dropout '+str(dropout)+' --num_heads '+str(num_heads)+' --num_layers '+str(num_layers)+' --num_bins '+str(num_bins)+' --weight_decay '+str(weight_decay)+' --hidden_dim '+str(hidden_dim)+' --end_token --start_token '+' --name_sufix '+str(name_sufix)+' --num_events_val '+str(num_events_val)+' --checkpoint_steps 1200000')
                                                     
                                                     
 
