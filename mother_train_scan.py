663016fc1d5af0b8a5a5dce7eaaaa7d8d186b5cd
HumbertosMac <you@example.com>
2024-04-24 19:07:54 +0200

    mother scan train

--- mother_train_scan.py
+++ mother_train_scan.py
@@ -140,7 +140,7 @@ num_bins_list=["41 31 31"]
 weight_decay_list=[0.00001]
 hidden_dim_list=[256]
 batch_size_list=[100]
-
+num_events_val=600
 ###Sampling parameters
 num_samples_test_list=[200]
 train_batch_size=100
@@ -195,7 +195,7 @@ for jet in list_of_jets:
                                                 
                                                     name_sufix=random_string()
                                                 
-                                                    os.system('python train_2.py --data_path '+str(data_path)+' --model_path '+str(model_path)+' --log_dir '+str(log_dir)+'  --output '+str(output)+' --num_const '+str(num_const)+' --num_epochs '+str(num_epochs)+'  --lr '+str(lr)+' --lr_decay '+str(lr_decay)+' --batch_size '+str(batch_size)+' --num_events '+str(num_events)+' --dropout '+str(dropout)+' --num_heads '+str(num_heads)+' --num_layers '+str(num_layers)+' --num_bins '+str(num_bins)+' --weight_decay '+str(weight_decay)+' --hidden_dim '+str(hidden_dim)+' --end_token --start_token '+' --name_sufix '+str(name_sufix))
+                                                    os.system('python train_2.py --data_path '+str(data_path)+' --model_path '+str(model_path)+' --log_dir '+str(log_dir)+'  --output '+str(output)+' --num_const '+str(num_const)+' --num_epochs '+str(num_epochs)+'  --lr '+str(lr)+' --lr_decay '+str(lr_decay)+' --batch_size '+str(batch_size)+' --num_events '+str(num_events)+' --dropout '+str(dropout)+' --num_heads '+str(num_heads)+' --num_layers '+str(num_layers)+' --num_bins '+str(num_bins)+' --weight_decay '+str(weight_decay)+' --hidden_dim '+str(hidden_dim)+' --end_token --start_token '+' --name_sufix '+str(name_sufix)+' --num_events_val '+str(num_events_val))
 
 
 
