b4ed245413c3047504b416743ccb125cc9880d6f
HumbertosMac <you@example.com>
2024-05-01 17:08:20 +0200

    train_classfier

---
+++ train_classifier_scan.py
@@ -0,0 +1,46 @@
+
+import os
+import numpy as np
+import h5py
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import string
+
+
+main_dir_discrete='/net/data_t2k/transformers-hep/JetClass/discretized/'
+
+sig_list=['TTBar_train___10M_TTBar.h5']
+bg_list=['ZJetsToNuNu_train___10M_ZJetsToNuNu.h5']
+num_epochs_list=[5]
+dropout_list=[0.0]
+num_heads_list=[4]
+num_layers_list=[8]
+hidden_dim_list=[256]
+batch_size_list=[100]
+num_events_list=[10000]
+num_const_list=[120]
+
+tag_of_train='top_vs_qcd_jetclass_classifier_test_1'
+log_dir='/net/data_t2k/transformers-hep/JetClass/Classification/'+tag_oftrain
+
+for sig in sig_list:
+    for bg in bg_list:
+        sig_path=main_dir_discrete+sig
+        bg_path=main_dir_discrete+bg
+
+        for num_events in num_events_list:
+            for num_const in num_const_list:
+                for batch_size in batch_size_list:
+                    for num_epochs in num_epochs_list:
+                                for num_layers in num_layers_list:
+                                    for dropout in dropout_list:
+                                        for num_heads in num_heads_list:
+                                            for lr in lr_list:
+                                                for hidden_dim in hidden_dim_list:
+                                                
+                                                
+                                                    name_sufix=random_string()
+                                                    train_command='python train_classifier.py   --log_dir '+str(log_dir)+' --bg '+str(bg_path)+' --sig '+str(sig_path)+' --num_const '+str(num_const)+' --num_epochs '+str(num_epochs)+'  --lr '+str(lr)+' --batch_size '+str(batch_size)+' --num_events '+str(num_events)+' --dropout '+str(dropout)+' --num_heads '+str(num_heads)+' --num_layers '+str(num_layers)+' --num_bins '+str(num_bins)+' --hidden_dim '+str(hidden_dim)+' --name_sufix '+str(name_sufix)
+                                                    os.system(train_command)
+
