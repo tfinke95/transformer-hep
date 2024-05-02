82c9e716c60c8fc987ca63ca40af8730a9947511
HumbertosMac <you@example.com>
2024-05-02 15:29:19 +0200

    test_joined

--- Workstation/test_joined.py
+++ Workstation/test_joined.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from data_eval_helpers import make_continues,Make_Plots,LoadTrue,LoadSGenamples,GetHighLevel,Wasserstein_distance
 import os
-
+import numpy as np
 
 
 def readFrameCont(path,n_samples):
