056fbe07dbfd6a8365f131cb70e8222591d2cf78
HumbertosMac <you@example.com>
2024-05-02 15:52:27 +0200

    test_joined

--- Workstation/test_joined.py
+++ Workstation/test_joined.py
@@ -17,13 +17,13 @@ pt_bins = np.load(bins_path_prefix+'pt_bins_'+bin_tag+'.npy')
 eta_bins = np.load(bins_path_prefix+'eta_bins_'+bin_tag+'.npy')
 phi_bins = np.load(bins_path_prefix+'phi_bins_'+bin_tag+'.npy')
 
-
+n_test_samples=200000
 file_name_samples='/net/data_t2k/transformers-hep/JetClass/TTBar_ZJetsToNuNu_models//TTBar_ZJetsToNuNu_run_scan_600k_XGNLMOG/samples__nsamples200000_trunc_5000.h5'
 
 jets,ptj,mj=LoadSGenamples(file_name_samples,pt_bins,eta_bins,phi_bins,n_test_samples)
 
 discrete_truedata_filename='/net/data_t2k/transformers-hep/JetClass/discretized/TTBar_ZJetsToNuNu_test___10M_TTBar_ZJetsToNuNu.h5'
-n_test_samples=200000
+
 jets_true,ptj_true,mj_true=LoadTrue(discrete_truedata_filename,n_test_samples,pt_bins,eta_bins,phi_bins)
 
 
