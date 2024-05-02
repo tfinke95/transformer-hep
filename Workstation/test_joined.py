7fbc5ad6fdda9cd7ed482cb6c1d57e0070b349f9
HumbertosMac <you@example.com>
2024-05-02 15:23:39 +0200

    test_joined

---
+++ Workstation/test_joined.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from data_eval_helpers import make_continues,Make_Plots,LoadTrue,LoadSGenamples,GetHighLevel,Wasserstein_distance
+
+
+
+
+def readFrameCont(path,n_samples):
+    tmp = pd.read_hdf(discrete_truedata_filename, key="discretized", stop=None)
+    tmp=tmb.sample(n_samples)
+    return tmp
+
+
+
+
+file_name_samples='/net/data_t2k/transformers-hep/JetClass/TTBar_ZJetsToNuNu_models//TTBar_ZJetsToNuNu_run_scan_600k_XGNLMOG/samples__nsamples200000_trunc_5000.h5'
+results=os.listdir(dir_name)
+jets,ptj,mj=LoadSGenamples(file_name_samples,pt_bins,eta_bins,phi_bins)
+
+discrete_truedata_filename='/net/data_t2k/transformers-hep/JetClass/discretized/TTBar_ZJetsToNuNu_test___10M_TTBar_ZJetsToNuNu.h5'
+n_test_samples=200000
+jets_true,ptj_true,mj_true=LoadTrue(discrete_truedata_filename,n_test_samples,pt_bins,eta_bins,phi_bins)
+
+bins_path_prefix='../preprocessing_bins/'
+bin_tag='10M_TTBar_ZJetsToNuNu'
+pt_bins = np.load(bins_path_prefix+'pt_bins_'+bin_tag+'.npy')
+eta_bins = np.load(bins_path_prefix+'eta_bins_'+bin_tag+'.npy')
+phi_bins = np.load(bins_path_prefix+'phi_bins_'+bin_tag+'.npy')
+
+path_to_plots='test_plots_join_'+str(n_test_samples)
+os.makedirs(path_to_plots,exist_ok=True)
+Make_Plots(jets,pt_bins,eta_bins,phi_bins,mj,jets_true,ptj_true,mj_true,path_to_plots)
