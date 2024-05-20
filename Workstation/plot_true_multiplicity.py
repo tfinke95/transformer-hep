1680278c7d59e6a52cdedfa082f63c86e1b5a9d2
HumbertosMac <you@example.com>
2024-05-20 13:23:15 +0200

    plot true mult

--- Workstation/plot_true_multiplicity.py
+++ Workstation/plot_true_multiplicity.py
@@ -16,6 +16,7 @@ def PlotMultiplicity(jets,color,jet):
     print(np.max(np.sum(mask, axis=1)))
     print(np.shape(np.sum(mask, axis=1)))
     print(np.sum(mask, axis=1))
+    print(np.sort(max))
     exit()
  
     plt.hist(np.sum(mask, axis=1), bins=np.linspace(-0.5, 200.5, 102),color=color,histtype='step',density=True,label=jet)
