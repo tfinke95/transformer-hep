6a7885c24f9ef94258f061f6c4eba4dc4ed5bb9d
HumbertosMac <you@example.com>
2024-05-20 13:54:35 +0200

    plot true mult

--- Workstation/plot_true_multiplicity.py
+++ Workstation/plot_true_multiplicity.py
@@ -61,7 +61,7 @@ pt_bins = np.load(bins_path_prefix+'pt_bins_'+bin_tag+'.npy')
 eta_bins = np.load(bins_path_prefix+'eta_bins_'+bin_tag+'.npy')
 phi_bins = np.load(bins_path_prefix+'phi_bins_'+bin_tag+'.npy')
 
-n_test_samples=10000
+n_test_samples=10000000
 
 
 
