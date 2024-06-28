48981504931b1637887f266b76edf08f0e2e54ee
HumbertosMac <you@example.com>
2024-06-28 18:18:29 +0200

    probs nconst optclass

--- probs_foroptclass_numconst_znunu_list.py
+++ probs_foroptclass_numconst_znunu_list.py
@@ -4,19 +4,19 @@ import os
 
 
 
-test_dataset='/net/data_t2k/transformers-hep/JetClass/TTBar_models//TTBar_run_testwall_10M_11/samples_samples_nsamples200000_trunc_5000.h5'
-test_dataset_other='/net/data_t2k/transformers-hep/JetClass/ZJetsToNuNu_models//ZJetsToNuNu_run_scan_10M_N1G96CW/samples__nsamples200000_trunc_5000.h5'
-mother_dir='/net/data_t2k/transformers-hep/JetClass/TTBar_models/'
+test_dataset='/net/data_t2k/transformers-hep/JetClass/ZJetsToNuNu_models//ZJetsToNuNu_run_scan_10M_N1G96CW/samples__nsamples200000_trunc_5000.h5'
+test_dataset_other='/net/data_t2k/transformers-hep/JetClass/TTBar_models//TTBar_run_testwall_10M_11/samples_samples_nsamples200000_trunc_5000.h5'
+mother_dir='/net/data_t2k/transformers-hep/JetClass/ZJetsToNuNu_models/'
 model_type='model_best.pt'
-tag_oftrain='TTBar_run_testwall_10M'
+tag_oftrain='ZJetsToNuNu_run_scan_10M'
 num_samples_list=[200000]
-train_batch_size=200
-num_const_list=[100,80,60]
+train_batch_size=100
+num_const_list=[100,80,60,40,20]
 trunc_list=[5000]
 ###For test samples
 bg=test_dataset
-bin_tag='10M_TTBar'
-list_of_results=[11]
+bin_tag='10M_ZJetsToNuNu'
+list_of_results=['N1G96CW']
 num_epochs_test=5
 
 models_list=os.listdir(mother_dir)
@@ -49,14 +49,14 @@ for result in list_of_results:
                     os.system(command_eval_other)
 
 
-                    command_sample= 'python sample_jets_2.py --model_dir '+model_path+' --savetag '+str(tag_forsample)+' --num_samples '+str(num_samples)+' --num_const '+str(num_const)+' --trunc '+str(trunc)+' --batchsize '+str(train_batch_size)
+                    command_sample= 'python sample_jets_0.py --model_dir '+model_path+' --savetag '+str(tag_forsample)+' --num_samples '+str(num_samples)+' --num_const '+str(num_const)+' --trunc '+str(trunc)+' --batchsize '+str(train_batch_size)
                     print(command_sample)
                     #os.system(command_sample)
         
         
                     sg=model_path+'samples_'+tag_forsample+'.h5'
                     save_dir=model_path+save_dir_tag
-                    command_test_sample='python test_samples_2.py --bg '+str(bg)+' --sig '+str(sg)+' --save_dir '+str(save_dir)+' --num_const '+str(num_const)+' --num_epochs '+str(num_epochs_test)+' -N '+str(num_samples)+' --bin_tag '+str(bin_tag)
+                    command_test_sample='python test_samples_0.py --bg '+str(bg)+' --sig '+str(sg)+' --save_dir '+str(save_dir)+' --num_const '+str(num_const)+' --num_epochs '+str(num_epochs_test)+' -N '+str(num_samples)+' --bin_tag '+str(bin_tag)
 
 
                     #os.system(command_test_sample)
