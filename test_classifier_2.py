a5c783617acf7650d63b58926352cae94acc1206
Humberto Reyes <hreyes@lx3agpu1.physik.rwth-aachen.de>
2024-07-08 19:04:49 +0200

    test classier optclass

---
+++ test_classifier_2.py
@@ -0,0 +1,199 @@
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import numpy as np
+from sklearn.metrics import roc_auc_score
+
+from preprocess import preprocess_dataframe
+
+from argparse import ArgumentParser
+from tqdm import tqdm
+import os
+import pandas as pd
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
+def load_model(name):
+    model = torch.load(os.path.join(args.model_dir, f'model_{name}.pt'))
+    return model
+
+
+def parse_input():
+    parser = ArgumentParser()
+    parser.add_argument("--model_dir", type=str, default='models/test', help="Model directory")
+    parser.add_argument("--data_path_1", type=str, default='/hpcwork/bn227573/top_benchmark/', help="Path to training data file")
+    parser.add_argument("--data_path_2", type=str, default='/hpcwork/bn227573/qcd_benchmark/', help="Path to training data file")
+
+    parser.add_argument("--num_workers", type=int, default=1, help="Number of workers")
+    parser.add_argument("--batch_size", type=int, default=128, help="Number of workers")
+
+    parser.add_argument("--num_const", type=int, default=100, help="Number of constituents")
+    parser.add_argument("--limit_const", action="store_true", help="Only use jets with at least num_const constituents")
+    parser.add_argument("--num_events", type=int, default=10000, help="Number of events for training")
+    parser.add_argument("--num_bins", type=int, nargs=3, default=[41, 31, 31], help="Number of bins per feature")
+    parser.add_argument("--reverse", action='store_true', help="Whether to reverse pt order")
+
+    parser.add_argument("--pred_name", type=str, default='predictions_test.npz', help="predicitons name")
+    
+    args = parser.parse_args()
+    return args
+
+
+def load_data(path1,path2, n_events):
+    df = pd.read_hdf(path1, 'discretized', stop=n_events)
+    x, padding_mask, _ = preprocess_dataframe(df, num_features=num_features,
+                                num_bins=num_bins,
+                                to_tensor=True,
+                                num_const=args.num_const,
+                                reverse=args.reverse,
+                                limit_nconst=args.limit_const)
+    labels = torch.ones(len(x))
+
+    print('load data')
+    print(x)
+    print(np.shape(x))
+    print(labels)
+    print(np.shape(labels))
+    
+    print(padding_mask)
+    print(np.shape(padding_mask))
+
+    extra=-1*np.ones((n_events,100-args.num_const,3))
+    extra=torch.as_tensor(np.array(extra),dtype=torch.int64)
+    x=torch.cat((x,extra),dim=1)
+    
+    extra_padding=np.zeros((n_events, 100-args.num_const), dtype=bool)    
+    extra_padding=torch.as_tensor(np.array(extra_padding),dtype=torch.bool)
+    padding_mask=torch.cat((padding_mask,extra_padding),dim=1)
+
+
+    df = pd.read_hdf(path2, 'discretized', stop=n_events)
+    x1, padding_mask1, _ = preprocess_dataframe(df, num_features=num_features,
+                                num_bins=num_bins,
+                                to_tensor=True,
+                                num_const=args.num_const,
+                                reverse=args.reverse,
+                                limit_nconst=args.limit_const)
+                                
+                  
+
+    extra=-1*np.ones((n_events,100-args.num_const,3))
+    extra=torch.as_tensor(np.array(extra),dtype=torch.int64)
+    x1=torch.cat((x1,extra),dim=1)
+                
+    
+    extra_padding=np.zeros((n_events, 100-args.num_const), dtype=bool)
+    extra_padding=torch.as_tensor(np.array(extra_padding),dtype=torch.bool)
+    padding_mask1=torch.cat((padding_mask1,extra_padding),dim=1)
+
+
+
+    labels = torch.concat((labels, torch.zeros(len(x1))))
+    x = torch.concat((x, x1), dim=0)
+    padding_mask = torch.concat((padding_mask, padding_mask1), dim=0)
+
+    train_dataset = TensorDataset(x, padding_mask, labels)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        shuffle=True,
+    )
+    print(x.shape)
+    return train_loader
+    
+    
+    
+
+def plot_roc_curve(y_true, y_score,model_dir):
+    # Compute ROC curve and ROC area for each class
+    fpr, tpr, _ = roc_curve(y_true, y_score)
+    roc_auc = auc(fpr, tpr)
+
+    # Plot ROC curve
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.legend(loc="lower right")
+    plt.savefig(model_dir+'/roc_test.png')
+    return
+
+
+def saveAUCscore(model_dir,auc_score):
+
+    file=open(model_dir+'/auc.txt','w')
+    file.write('auc_score')
+    file.write('\n')
+    file.write(str(auc_score))
+    file.write('\n')
+    file.close()
+
+    return
+
+if __name__ == '__main__':
+    args = parse_input()
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"Running on device: {device}")
+
+    num_features = 3
+    num_bins = tuple(args.num_bins)
+
+    print(f"Using bins: {num_bins}")
+    print(f"{'Not' if not args.reverse else ''} reversing pt order")
+
+    # load and preprocess data
+    print(f"Loading test set")
+    test_loader = load_data(args.data_path_1,args.data_path_2 , args.num_events)
+
+    # construct model
+    model = load_model('best')
+    print("Loaded model")
+    model.to(device)
+    model.eval()
+
+    loss_list = []
+    prediction_list = []
+    label_list = []
+    min_val_loss = np.inf
+
+    for x, padding_mask, label in tqdm(test_loader, total=len(test_loader), desc=f'Testing'):
+        label_list.append(label.detach().numpy())
+        x = x.to(device)
+        padding_mask = padding_mask.to(device)
+        label = label.to(device)
+
+        with torch.no_grad():
+            with torch.cuda.amp.autocast():
+                logits = model(x, padding_mask)
+                predictions = torch.sigmoid(logits)
+                loss = model.loss(logits, label.view(-1, 1))
+
+        loss_list.append(loss.cpu().detach().numpy())
+        prediction_list.append(predictions.cpu().detach().numpy())
+
+    predictions = np.concatenate(prediction_list, axis=0)[:,0]
+    label_list = np.concatenate(label_list, axis=0)
+    print(predictions.shape)
+    print(label_list.shape)
+    print(predictions)
+    print(label_list)
+    
+    auc_score=roc_auc_score(label_list, predictions)
+    print(auc_score)
+    
+    plot_roc_curve(label_list, predictions,args.model_dir)
+    saveAUCscore(args.model_dir,auc_score)
+    
+    np.savez(os.path.join(args.model_dir, args.pred_name),
+            predictions=predictions,
+            labels=label_list)
