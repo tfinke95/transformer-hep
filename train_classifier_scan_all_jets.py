ed358cf7498a205e46dbce64adc43a2e3fc88ec5
HumbertosMac <you@example.com>
2024-07-19 14:03:47 +0200

    train_classifier_scan_all_jets

--- train_classifier_scan_all_jets.py
+++ train_classifier_scan_all_jets.py
@@ -21,19 +21,19 @@ def random_string():
     
 main_dir_discrete='/net/data_t2k/transformers-hep/JetClass/discretized/'
 
-sig_list=['TTBar_train___10M_TTBar.h5']
+sig_list=['ZToQQ_train___10M_ZToQQ.h5']
 bg_list=['ZJetsToNuNu_train___10M_ZJetsToNuNu.h5']
 num_epochs_list=[30]
 dropout_list=[0.0]
 num_heads_list=[4]
 num_layers_list=[8]
 hidden_dim_list=[256]
-batch_size_list=[300]
-num_events_list=[1000,10000,100000,500000]
+batch_size_list=[100]
+num_events_list=[10000,100000,1000000,1000]
 num_const_list=[100]
 lr_list=[.001]
 
-tag_of_train='top_vs_qcd_jetclass_classifier_test_1'
+tag_of_train='z_vs_qcd_jetclass_classifier_test_1'
 log_dir='/net/data_t2k/transformers-hep/JetClass/Classification/'+tag_of_train
 
 for sig in sig_list:
@@ -53,6 +53,6 @@ for sig in sig_list:
                                                 
                                                 
                                                     name_sufix=random_string()
-                                                    train_command='python train_classifier_3.py   --log_dir '+str(log_dir)+' --bg '+str(bg_path)+' --sig '+str(sig_path)+' --num_const '+str(num_const)+' --num_epochs '+str(num_epochs)+'  --lr '+str(lr)+' --batch_size '+str(batch_size)+' --num_events '+str(num_events)+' --dropout '+str(dropout)+' --num_heads '+str(num_heads)+' --num_layers '+str(num_layers)+' --hidden_dim '+str(hidden_dim)+' --name_sufix '+str(name_sufix)
+                                                    train_command='python train_classifier_new.py   --log_dir '+str(log_dir)+' --bg '+str(bg_path)+' --sig '+str(sig_path)+' --num_const '+str(num_const)+' --num_epochs '+str(num_epochs)+'  --lr '+str(lr)+' --batch_size '+str(batch_size)+' --num_events '+str(num_events)+' --dropout '+str(dropout)+' --num_heads '+str(num_heads)+' --num_layers '+str(num_layers)+' --hidden_dim '+str(hidden_dim)+' --name_sufix '+str(name_sufix)
                                                     os.system(train_command)
 
