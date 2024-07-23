79be982ffb3023cc86f36d9b27182bb31c0ad5da
HumbertosMac <you@example.com>
2024-07-23 14:59:59 +0200

    retrain gen

---
+++ mother_retrain_gen_scan_all_0.py
@@ -0,0 +1,267 @@
+import os
+import numpy as np
+import h5py
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import string
+###make sufix a random number
+
+
+def make_continues(jets, mask,pt_bins,eta_bins,phi_bins, noise=False):
+
+
+    pt_disc = jets[:, :, 0]
+    eta_disc = jets[:, :, 1]
+    phi_disc = jets[:, :, 2]
+
+    if noise:
+        pt_con = (pt_disc - np.random.uniform(0.0, 1.0, size=pt_disc.shape)) * (
+            pt_bins[1] - pt_bins[0]
+        ) + pt_bins[0]
+        eta_con = (eta_disc - np.random.uniform(0.0, 1.0, size=eta_disc.shape)) * (
+            eta_bins[1] - eta_bins[0]
+        ) + eta_bins[0]
+        phi_con = (phi_disc - np.random.uniform(0.0, 1.0, size=phi_disc.shape)) * (
+            phi_bins[1] - phi_bins[0]
+        ) + phi_bins[0]
+    else:
+        pt_con = (pt_disc - 0.5) * (pt_bins[1] - pt_bins[0]) + pt_bins[0]
+        eta_con = (eta_disc - 0.5) * (eta_bins[1] - eta_bins[0]) + eta_bins[0]
+        phi_con = (phi_disc - 0.5) * (phi_bins[1] - phi_bins[0]) + phi_bins[0]
+
+
+    pt_con = np.exp(pt_con)
+    pt_con[mask] = 0.0
+    eta_con[mask] = 0.0
+    phi_con[mask] = 0.0
+    
+    pxs = np.cos(phi_con) * pt_con
+    pys = np.sin(phi_con) * pt_con
+    pzs = np.sinh(eta_con) * pt_con
+    es = (pxs ** 2 + pys ** 2 + pzs ** 2) ** (1. / 2)
+
+    pxj = np.sum(pxs, -1)
+    pyj = np.sum(pys, -1)
+    pzj = np.sum(pzs, -1)
+    ej = np.sum(es, -1)
+    
+    ptj = np.sqrt(pxj**2 + pyj**2)
+    mj = (ej ** 2 - pxj ** 2 - pyj ** 2 - pzj ** 2) ** (1. / 2)
+
+    continues_jets = np.stack((pt_con, eta_con, phi_con), -1)
+
+    return continues_jets, ptj, mj
+
+def Make_Plots(jets_gen,mj_gen,pt_bins,eta_bins,phi_bins,jets_true,ptj_true,mj_true,path_to_plots,plot_title):
+    print('making plots')
+    plt.hist(np.log(jets_gen[:,:,0]).flatten(), bins=pt_bins, color='black',histtype='step',density=True,label="Gen")
+    plt.hist(np.log(jets_true[:,:,0]).flatten(), bins=pt_bins, color='red',histtype='step',density=True,label="True")
+    plt.legend()
+    plt.title(plot_title,loc='left')
+    plt.xlabel('$\log (p_T)$')
+    
+    plt.savefig(path_to_plots+'plot_pt_trans_all.png')
+    plt.close()
+
+    mask_tr = jets_true[:, :, 0] != 0
+    mask_eta_true = jets_true[mask_tr, 1]
+    
+    mask_gen = jets_gen[:, :, 0] != 0
+    mask_eta_gen = jets_gen[mask_gen, 1]
+    
+    
+    plt.hist(mask_eta_gen, bins=eta_bins, color='black',histtype='step',density=True,label="Gen")
+    plt.hist(mask_eta_true, bins=eta_bins, color='red',histtype='step',density=True,label="True")
+    plt.xlabel('$\Delta\eta$')
+    plt.legend()
+    plt.title(plot_title,loc='left')
+    plt.savefig(path_to_plots+'plot_eta_trans_all.png')
+    plt.close()
+    
+    
+    mask_tr = jets_true[:, :, 0] != 0
+    mask_phi_true = jets_true[mask_tr,2]
+    
+    mask_gen = jets_gen[:, :, 0] != 0
+    mask_phi_gen = jets_gen[mask_gen,2]
+    
+    plt.hist(mask_phi_gen, bins=phi_bins, color='black',histtype='step',density=True,label="Gen")
+    plt.hist(mask_phi_true, bins=phi_bins, color='red',histtype='step',density=True,label="True")
+    plt.xlabel('$\Delta\phi$')
+    plt.legend()
+    plt.title(plot_title,loc='left')
+    plt.savefig(path_to_plots+'plot_phi_trans_all.png')
+    
+    plt.close()
+    
+    mask = jets_gen[:, :, 0] != 0
+    plt.hist(np.sum(mask, axis=1), bins=np.linspace(-0.5, 100.5, 102),color='blue',histtype='step',density=True,label="Gen")
+    
+    
+
+    
+    
+    mask = jets_true[:, :, 0] != 0
+    plt.hist(np.sum(mask, axis=1), bins=np.linspace(-0.5, 150.5, 152),color='red',histtype='step',density=True,label="True")
+    plt.legend()
+    plt.title(plot_title,loc='left')
+    plt.xlabel('Multiplicity')
+    plt.savefig(path_to_plots+'plot_mul_trans_all.png')
+    plt.close()
+    
+    
+    mj_bins = np.linspace(0, 450, 100)
+    
+
+    plt.hist(np.clip(mj_gen, mj_bins[0], mj_bins[-1]), bins=mj_bins,color='black',histtype='step',density=True,label="Gen")
+    
+    
+    plt.hist(np.clip(mj_true, mj_bins[0], mj_bins[-1]), bins=mj_bins,color='red',histtype='step',density=True,label="True")
+    plt.yscale('log')
+    
+    plt.legend()
+    plt.title(plot_title,loc='left')
+    plt.xlabel('$m_{jet}$')
+    plt.savefig(path_to_plots+'plot_mj_trans_all.png')
+    plt.close()
+    return
+
+
+def LoadTrue(discrete_truedata_filename,n_samples):
+
+
+    tmp = pd.read_hdf(discrete_truedata_filename, key="discretized", stop=None)
+    print(tmp)
+    print(tmp.shape)
+    
+    tmp = tmp.to_numpy()[:, :600].reshape(len(tmp), -1, 3)
+    print(tmp)
+    print(tmp.shape)
+    print('hello')
+    tmp=tmp[:n_samples,:,:]
+    print(tmp.shape)
+    print('hello')
+ 
+
+    mask = tmp[:, :, 0] == -1
+    print(mask)
+    jets_true,ptj_true,mj_true = make_continues(tmp, mask,pt_bins,eta_bins,phi_bins, noise=False)
+
+    return jets_true,ptj_true,mj_true
+def random_string():
+    # initializing size of string
+    N = 7
+ 
+    # using random.choices()
+    # generating random strings
+    res = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k=N))
+    # print result
+    print("The generated random string : " + str(res))
+    return str(res)
+####Trainning parameters
+model_path_in='/net/data_t2k/transformers-hep/JetClass/All_models_for_OptClass//all_const_403030_ZJetsToNuNu/'
+list_of_jets=['HToCC']
+#list_of_jets=['TTBar','ZJetsToNuNu','HToBB','HToCC','HToGG','HToWW2Q1L','HToWW4Q','TTBarLep','WToQ','ZToQQ']
+#list_of_jets=['ZJetsToNuNu']
+num_const_list=[200]
+num_epochs_list=[3]
+lr_list=[.001]
+lr_decay_list=[.000001]
+num_events_list=[1000]
+dropout_list=[0]
+num_heads_list=[4]
+num_layers_list=[8]
+num_bins_list=["41 31 31"]
+weight_decay_list=[0.00001]
+hidden_dim_list=[256]
+batch_size_list=[100]
+num_events_val=500000
+###Sampling parameters
+num_samples_test_list=[200]
+train_batch_size=100
+num_const_test=200
+trunc_test_list=[5000]
+model_name='model_best.pt'
+
+
+main_dir='/net/data_t2k/transformers-hep/JetClass/'
+bins_path_prefix='preprocessing_bins/'
+###Training stage
+for jet in list_of_jets:
+
+    mother_dir='/net/data_t2k/transformers-hep/JetClass/'+jet+'_models/'
+    tag_oftrain=jet+'_finetunefromQCD_const190_403030'
+    data_path='/net/data_t2k/transformers-hep/JetClass/discretized/'+jet+'_train___10M_'+jet+'.h5'
+    model_path=mother_dir+'/'+tag_oftrain
+    log_dir='/net/data_t2k/transformers-hep/JetClass/'+jet+'_models/'+tag_oftrain
+    output='linear'
+    
+    ###for sampling
+    
+
+    test_dataset='/net/data_t2k/transformers-hep/JetClass/discretized/'+jet+'_test___10M_'+jet+'.h5'
+    
+    
+    ###for plotting samples
+
+    bin_tag='10M_'+jet
+    pt_bins = np.load(bins_path_prefix+'pt_bins_'+bin_tag+'.npy')
+    eta_bins = np.load(bins_path_prefix+'eta_bins_'+bin_tag+'.npy')
+    phi_bins = np.load(bins_path_prefix+'phi_bins_'+bin_tag+'.npy')
+
+    
+    test_data_name='/discretized/'+jet+'_test___10M_'+jet+'.h5'
+    discrete_truedata_filename=main_dir+test_data_name
+    
+    for n_samples in num_samples_test_list:
+    
+        jets_true,ptj_true,mj_true=LoadTrue(discrete_truedata_filename,n_samples)
+        print(mj_true)
+
+
+        for num_events  in num_events_list:
+            for num_const in num_const_list:
+                for batch_size in batch_size_list:
+                    for num_bins in num_bins_list:
+                        for num_epochs in num_epochs_list:
+              
+                            for weight_decay in weight_decay_list:
+     
+                                            for lr in lr_list:
+                                 
+                                                
+                                                
+                                                    name_sufix=random_string()
+                                                
+                                                    os.system('python re_train_gen.py --model_path_in '+str(model_path_in)+' --model_name '+str(model_name)+' --data_path '+str(data_path)+' --model_path '+str(model_path)+' --log_dir '+str(log_dir)+'  --output '+str(output)+' --num_const '+str(num_const)+' --num_epochs '+str(num_epochs)+'  --lr '+str(lr)+' --batch_size '+str(batch_size)+' --num_events '+str(num_events)+' --num_bins '+str(num_bins)+' --weight_decay '+str(weight_decay)+' --end_token --start_token '+' --name_sufix '+str(name_sufix)+' --num_events_val '+str(num_events_val)+' --checkpoint_steps 1200000 --contin')
+                                                    
+                                                    
+                                                    model_path_curr=model_path+'_'+name_sufix
+                                                    for trunc in trunc_test_list:
+                                                            
+                                                            tag_forsample='_nsamples'+str(num_samples_test)+'_trunc_'+str(trunc)
+                                                            command_sample= 'python sample_jets_2.py --model_dir '+model_path_curr+' --savetag '+str(tag_forsample)+' --num_samples '+str(num_samples_test)+' --num_const '+str(num_const_test)+' --trunc '+str(trunc)+' --batchsize '+str(train_batch_size)+' --model_name '+model_name
+                                                            print(command_sample)
+                                                            os.system(command_sample)
+                                                    
+                                                    
+                                                    
+                                                            ####plotting
+                                                            filename=model_path_curr+'/samples_'+tag_forsample+'.h5'
+
+                                                            path_to_plots=model_path_curr+'/'+tag_forsample
+                                                            os.makedirs(path_to_plots,exist_ok=True)
+
+                                                            #filename = test_results_dir+"/ttbar_run_b_2_6/samples_test_sample_200k.h5"
+                                                            tmp = pd.read_hdf(filename, key="discretized", stop=None)
+                                                            tmp = tmp.to_numpy()[:, :600].reshape(len(tmp), -1, 3)
+                                                            print(tmp.shape)
+
+                                                            mask = tmp[:, :, 0] == -1
+                                                            jets,ptj,mj = make_continues(tmp, mask,pt_bins,eta_bins,phi_bins, noise=False)
+
+                                                            Make_Plots(jets,pt_bins,eta_bins,phi_bins,mj,jets_true,ptj_true,mj_true,path_to_plots)
+
+                                          
---
+++ re_train_gen.py
@@ -0,0 +1,237 @@
+import torch
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import time, os
+from argparse import ArgumentParser
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
+from model import JetTransformer
+
+from tqdm import tqdm
+from helpers_train import *
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+torch.multiprocessing.set_sharing_strategy("file_system")
+
+
+def set_seeds(seed):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+
+parser = ArgumentParser()
+parser.add_argument("--model_path_in", type=str, default="models/test")
+parser.add_argument("--model_name", type=str, default="model_best.pt")
+
+#parser.add_argument("--num_samples", type=int, default=1000)
+parser.add_argument("--batchsize", type=int, default=100)
+parser.add_argument("--num_const", type=int, default=50)
+parser.add_argument("--seed", type=int, default=0)
+#parser.add_argument("--trunc", type=float, default=None)
+parser.add_argument("--lr", type=float, default=.001)
+
+parser.add_argument("--weight_decay", type=float, default=.00001)
+parser.add_argument("--num_epochs", type=float, default=30)
+
+parser.add_argument(
+        "--name_sufix", type=str, default="A1B2C3D", help="name of train dir"
+    )
+
+parser.add_argument(
+        "--data_path",
+        type=str,
+        default="/hpcwork/bn227573/top_benchmark/train_qcd_30_bins.h5",
+        help="Path to training data file",
+    )
+
+
+
+#parser.add_argument( "--model_path", type=str,default=None help="Model file to load")
+parser.add_argument(
+        "--num_events", type=int, default=10000, help="Number of events for training"
+    )
+    
+parser.add_argument(
+        "--num_events_val", type=int, default=500000, help="Number of events for training"
+    )
+
+parser.add_argument(
+        "--num_bins",
+        type=int,
+        nargs=3,
+        default=[41, 31, 31],
+        help="Number of bins per feature",
+    )
+parser.add_argument(
+        "--reverse", action="store_true", help="Whether to reverse pt order"
+    )
+parser.add_argument(
+        "--start_token",
+        action="store_true",
+        help="Whether to use a start particle (learn first particle as well)",
+    )
+parser.add_argument(
+        "--end_token",
+        action="store_true",
+        help="Whether to use a end particle (learn jet length as well)",
+    )
+
+parser.add_argument(
+        "--limit_const",
+        action="store_true",
+        help="Only use jets with at least num_const constituents",
+    )
+
+
+parser.add_argument("--num_workers", type=int, default=4, help="Number of workers")
+
+parser.add_argument(
+        "--log_dir", type=str, default="models/test", help="Model directory"
+    )
+
+parser.add_argument(
+        "--checkpoint_steps", type=int, default=100000, help="Model directory"
+    )
+
+parser.add_argument(
+        "--contin",      action="store_true",
+        help="continue training",
+    )
+
+args = parser.parse_args()
+
+set_seeds(args.seed)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+assert device == "cuda", "Not running on GPU"
+
+print('limit_const')
+print(args.limit_const)
+num_features = 3
+train_loader = load_data(
+        path=args.data_path,
+        n_events=args.num_events,
+        num_features=num_features,
+        num_bins=num_bins,
+        num_const=args.num_const,
+        reverse=args.reverse,
+        start_token=args.start_token,
+        end_token=args.end_token,
+        limit_const=args.limit_const,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+    )
+
+
+
+
+#n_batches = args.num_samples // args.batchsize
+#rest = args.num_samples % args.batchsize
+
+# Load model for sampling
+model = torch.load(os.path.join(args.model_path_in, args.model_name))
+model.classifier = False
+model.to(device)
+
+
+# construct optimizer and auto-caster
+opt = torch.optim.Adam(
+        model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+scheduler = get_cos_scheduler(args.num_epochs, len(train_loader), opt)
+scaler = torch.cuda.amp.GradScaler()
+
+if args.contin:
+    load_opt_states_best(opt, scheduler, scaler, args.model_path_in)
+    print("Loaded optimizer")
+
+logger = SummaryWriter(args.log_dir)
+global_step = args.global_step
+loss_list = []
+perplexity_list = []
+mean_val_loss=9999999
+for epoch in range(args.num_epochs):
+    model.train()
+
+    for x, padding_mask, true_bin in tqdm(
+        train_loader, total=len(train_loader), desc=f"Training Epoch {epoch + 1}"
+    ):
+        opt.zero_grad()
+        x = x.to(device)
+        padding_mask = padding_mask.to(device)
+        true_bin = true_bin.to(device)
+
+        with torch.cuda.amp.autocast():
+            logits = model(x, padding_mask)
+            loss = model.loss(logits, true_bin)
+            with torch.no_grad():
+                perplexity = model.probability(
+                    logits,
+                    padding_mask,
+                    true_bin,
+                    perplexity=True,
+                    logarithmic=False,
+                    topk=False
+                )
+
+        scaler.scale(loss).backward()
+        scaler.step(opt)
+        scaler.update()
+        scheduler.step()
+
+        loss_list.append(loss.cpu().detach().numpy())
+        perplexity_list.append(perplexity.mean().cpu().detach().numpy())
+
+        if (global_step + 1) % args.logging_steps == 0:
+            logger.add_scalar("Train/Loss", np.mean(loss_list), global_step)
+            logger.add_scalar(
+                "Train/Perplexity", np.mean(perplexity_list), global_step
+            )
+            logger.add_scalar("Train/LR", scheduler.get_last_lr()[0], global_step)
+            loss_list = []
+            perplexity_list = []
+
+        if (args.checkpoint_steps != 0) and (
+            (global_step + 1) % args.checkpoint_steps == 0
+        ):
+            save_model(model, args.log_dir, f"checkpoint_{global_step + 1}")
+
+        global_step += 1
+
+    model.eval()
+    with torch.no_grad():
+        val_loss = []
+        val_perplexity = []
+        for x, padding_mask, true_bin in tqdm(
+            val_loader, total=len(val_loader), desc=f"Validation Epoch {epoch + 1}"
+        ):
+            x = x.to(device)
+            padding_mask = padding_mask.to(device)
+            true_bin = true_bin.to(device)
+
+            logits = model(
+                x,
+                padding_mask,
+            )
+            loss = model.loss(logits, true_bin)
+            perplexity = model.probability(
+                logits, padding_mask, true_bin, perplexity=True, logarithmic=False
+            )
+            val_loss.append(loss.cpu().detach().numpy())
+            val_perplexity.append(perplexity.mean().cpu().detach().numpy())
+
+        logger.add_scalar("Val/Loss", np.mean(val_loss), global_step)
+        logger.add_scalar("Val/Perplexity", np.mean(val_perplexity), global_step)
+    
+    if np.mean(val_loss) < mean_val_loss:
+            print('new val loss:'+str(np.mean(val_loss))+'<'+str(mean_val_loss)+' saving new model as best' )
+            save_model(model, args.log_dir, "best")
+            mean_val_loss=np.mean(val_loss)
+            save_opt_states_best(opt, scheduler, scaler, args.log_dir)
+        
+    save_model(model, args.log_dir, "last")
+    save_opt_states(opt, scheduler, scaler, args.log_dir)
