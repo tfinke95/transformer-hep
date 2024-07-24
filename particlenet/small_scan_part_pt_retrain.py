d22e46a911d4e0d499b4122f2dd82adc412842bd
HumbertosMac <you@example.com>
2024-07-24 19:02:40 +0200

    pnet part_pt_retrain

--- particlenet/small_scan_part_pt_retrain.py
+++ particlenet/small_scan_part_pt_retrain.py
@@ -70,7 +70,7 @@ for sig_dir in sig_dirs:
 
     
     config_dict=OpenConfig(config_file)
-    out_dir='logs/retrain_gen_ttbar_50k_test_'+str(sig_dir)
+    out_dir='logs/retrain_gen_ttbar_50k_test2_'+str(sig_dir)
     UpdateConfig(config_dict,sig_file,bg_file,out_dir)
     SaveNewConfig(config_dict,config_file)
     train(config_file)
