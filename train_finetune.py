39f7a38b86b452703f2e2119666c8d141eec351c
HumbertosMac <you@example.com>
2024-08-26 18:09:14 +0200

    train finetune

--- train_finetune.py
+++ train_finetune.py
@@ -443,7 +443,7 @@ if __name__ == "__main__":
     filtered_opt_state_dict= AddLayersToDict(filtered_opt_state_dict,last2state,last2paramgroups,last_keys)
     opt.load_state_dict(filtered_opt_state_dict)
     
-    print(opt.load_state_dict(filtered_opt_state_dict))
+    print(opt.state_dict())
     scheduler = get_cos_scheduler(
         num_epochs=args.num_epochs,
         num_batches=len(train_loader),
