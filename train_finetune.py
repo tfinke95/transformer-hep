60013974e7efac0418179e0c8ddfd520cbcd8664
HumbertosMac <you@example.com>
2024-08-26 17:52:53 +0200

    train finetune

--- train_finetune.py
+++ train_finetune.py
@@ -387,8 +387,8 @@ def AddLayersToDict(filtered_sate_dict,last2state,last2paramgroups,last_keys):
 
     filtered_sate_dict.get('param_groups')[0].get('params').append(last2paramgroups[0])
     filtered_sate_dict.get('param_groups')[0].get('params').append(last2paramgroups[1])
-    filtered_sate_dict['state'][last_keys[0]]=last2state[0]
-    filtered_sate_dict['state'][last_keys[1]]=last2state[1]
+    #filtered_sate_dict['state'][last_keys[0]]=last2state[0]
+    #filtered_sate_dict['state'][last_keys[1]]=last2state[1]
     print(last2state[1])
     print(filtered_sate_dict['state'])
     print(filtered_sate_dict['param_groups'])
