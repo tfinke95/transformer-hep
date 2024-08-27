90f9badc76a41cd2f1283a5bdc7975b76e2711db
HumbertosMac <you@example.com>
2024-08-27 16:08:42 +0200

    train_classifier_scan_onebinning_multigpu

--- train_classifier_multigpu.py
+++ train_classifier_multigpu.py
@@ -167,7 +167,7 @@ def get_dataloader(
     train_loader = DataLoader(
         train_set,
         batch_size=args.batch_size,
-        shuffle=True,
+        #shuffle=True,
         sampler=train_sampler
     )
     val_loader = DataLoader(
