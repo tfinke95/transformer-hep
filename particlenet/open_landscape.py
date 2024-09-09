508b97633b8e6f929414e55122bebf45f6274b45
HumbertosMac <you@example.com>
2024-09-09 16:43:04 +0200

    pnet landscape

--- particlenet/open_landscape.py
+++ particlenet/open_landscape.py
@@ -8,10 +8,12 @@ file_path = '/home/home3/institut_thp/hreyes/Transformers/transformers_finke/dat
 
 
 dat_train = pd.read_hdf(file_path, key='raw')
+dat_train_disc = pd.read_hdf(file_path, key='discretized')
 print(dat_train)
 
 file_path_val = '/home/home3/institut_thp/hreyes/Transformers/transformers_finke/datasets/TopQuarkTag/topquarktagging_data/discretized//val_top_40_30_30_forall.h5'
 dat_val = pd.read_hdf(file_path_val, key='raw')
+dat_val_disc = pd.read_hdf(file_path_val, key='discretized')
 print(dat_val)
 
 
@@ -19,5 +21,12 @@ file_path_all = '/home/home3/institut_thp/hreyes/Transformers/transformers_finke
 data_all=pd.concat([dat_train,dat_val],axis=0)
 print(data_all.shape)
 
-data_all.to_csv(file_path_all,index=False)
+data_all_disc=pd.concat([dat_train_disc,dat_val_disc],axis=0)
+
+#data_all.to_csv(file_path_all,index=False)
+
+data_all.to_hdf(file_path_all, key="raw", mode="w", complevel=9)
+data_all_disc.to_hdf(file_path_all, key="discretized", mode="r+", complevel=9)
+
+
 print(data_all)
