590e725101c0ff3bdbfb50582c2e9a121a45b3cf
HumbertosMac <you@example.com>
2024-09-09 15:56:05 +0200

    pnet landscape

--- particlenet/open_landscape.py
+++ particlenet/open_landscape.py
@@ -8,11 +8,11 @@ file_path = '/home/home3/institut_thp/hreyes/Transformers/transformers_finke/dat
 
 
 dat_train = pd.read_hdf(file_path, key='raw')
-print(dat)
+print(dat_train)
 
 file_path_val = '/home/home3/institut_thp/hreyes/Transformers/transformers_finke/datasets/TopQuarkTag/topquarktagging_data/discretized//val_qcd_all_bins403030.h5'
 dat_val = pd.read_hdf(file_path, key='raw')
-
+print(dat_val)
 
 
 file_path_all = '/home/home3/institut_thp/hreyes/Transformers/transformers_finke/datasets/TopQuarkTag/topquarktagging_data/discretized//all_qcd_all_bins403030.h5'
@@ -20,3 +20,4 @@ data_all=pd.concat([dat_train,dat_val],axis=0)
 print(data_all.shape)
 
 data_all.to_csv(file_path_all,index=False)
+print(dat_all)
