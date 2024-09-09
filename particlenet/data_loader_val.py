4fa50153534252787ac5eea401ba67619b6b32ec
HumbertosMac <you@example.com>
2024-09-09 15:04:59 +0200

    landscape pnet

---
+++ particlenet/data_loader_val.py
@@ -0,0 +1,266 @@
+import numpy as np
+import sys, json, os
+import pandas as pd
+
+
+def transform_momenta(momenta, mask):
+    pts = momenta[:, :, 0]
+    etas = momenta[:, :, 1]
+    phis = momenta[:, :, 2]
+
+    drs = np.sqrt(np.sum(np.square(momenta[:, :, 1:3]), -1))
+
+    pxs = np.cos(phis) * pts
+    pys = np.sin(phis) * pts
+    pxs[~mask] = 0
+    pys[~mask] = 0
+
+    pxj = np.sum(pxs, -1)
+    pyj = np.sum(pys, 1)
+    ptj = np.sqrt(pxj**2 + pyj**2).reshape(-1, 1)
+    
+    etas[~mask] = 0
+    phis[~mask] = 0
+    #pts[~mask] = 0
+    #logpts = np.log(pts+1e-90)
+    #logpt_ptj = np.log(pts / ptj.reshape(-1, 1))
+    #print(np.count_nonzero(np.isinf(logpts)))
+    #print('inf back')
+    #logpts[~mask] = 0
+    #logpt_ptj = np.log(pts+1e-9)
+    #pts=pts+1e-40
+    neg_pts_num=(pts<=0).sum()
+    print('neg_pts')
+    print(neg_pts_num)
+    
+    neg_pts=pts[pts<=0]
+    
+    print(neg_pts)
+ 
+    logpts = np.log(pts)
+    logpts[~mask] = 0
+    logpt_ptj = np.log( pts/ptj.reshape(-1, 1))
+    #print(np.count_nonzero(np.isinf(logpts)))
+    #print('inf back')
+    
+    logpt_ptj[~mask] = 0
+    #logpt_ptj[~mask] = 0
+    
+    drs[~mask] = 0
+    #print(np.count_nonzero(np.isinf(logpt_ptj)))
+    #newVec = np.stack(
+    #    [etas, phis, logpts, logpt_ptj, drs],
+    #    -1,
+    #)
+
+    newVec = np.stack(
+        [etas, phis, logpts, logpt_ptj, drs],
+        -1,
+    )
+
+    return newVec
+
+
+def get_config(test=False):
+    if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]):
+        config = json.load(open(sys.argv[1], "r"))
+    else:
+        assert not test, f"Provide config file used for training"
+        config = json.load(open("config.json", "r"))
+
+    if not test:
+        i = 0
+        original = config["logging"]["logfolder"]
+        while os.path.isdir(config["logging"]["logfolder"]):
+            i += 1
+            config["logging"]["logfolder"] = f"{original}_{i}"
+
+        os.makedirs(config["logging"]["logfolder"])
+        json.dump(
+            config,
+            open(os.path.join(config["logging"]["logfolder"], "config.json"), "w"),
+            sort_keys=True,
+            indent=2,
+        )
+
+    return config
+
+
+def make_continues(jets,jet_type, mask,bins_tag, noise=False):
+    if jet_type=='sig':
+
+
+        pt_bins = np.load("../preprocessing_bins/pt_bins_"+bins_tag+".npy")
+        eta_bins = np.load("../preprocessing_bins/eta_bins_"+bins_tag+".npy")
+        phi_bins = np.load("../preprocessing_bins/phi_bins_"+bins_tag+".npy")
+    if jet_type=='bg':
+
+        pt_bins = np.load("../preprocessing_bins/pt_bins_"+bins_tag+".npy")
+        eta_bins = np.load("../preprocessing_bins/eta_bins_"+bins_tag+".npy")
+        phi_bins = np.load("../preprocessing_bins/phi_bins_"+bins_tag+".npy")
+
+
+
+    pt_disc = jets[:, :, 0]
+    eta_disc = jets[:, :, 1]
+    phi_disc = jets[:, :, 2]
+
+    if noise:
+        pt_con = (pt_disc - np.random.uniform(0.0, 1.0, size=pt_disc.shape)) * (
+            pt_bins[1] - pt_bins[0]
+        ) + pt_bins[0]
+        eta_con = (eta_disc - np.random.uniform(0.0, 1.0, size=eta_disc.shape)) * (
+            eta_bins[1] - eta_bins[0]
+        ) + eta_bins[0]
+        phi_con = (phi_disc - np.random.uniform(0.0, 1.0, size=phi_disc.shape)) * (
+            phi_bins[1] - phi_bins[0]
+        ) + phi_bins[0]
+    else:
+        pt_con = (pt_disc - 0.5) * (pt_bins[1] - pt_bins[0]) + pt_bins[0]
+        eta_con = (eta_disc - 0.5) * (eta_bins[1] - eta_bins[0]) + eta_bins[0]
+        phi_con = (phi_disc - 0.5) * (phi_bins[1] - phi_bins[0]) + phi_bins[0]
+
+
+    pt_con = np.exp(pt_con)
+    pt_con[~mask] = 0.0
+    eta_con[~mask] = 0.0
+    phi_con[~mask] = 0.0
+
+    return np.stack((pt_con, eta_con, phi_con), -1)
+
+
+def load_data(params, test=False, plot_dists=None,):
+    def load_file(file, key=None):
+        # Load  sample file
+        if file.endswith("npz"):
+            if test:
+                file = file.replace("train_", "test_")
+            print(f"\nChanged file to {file}")
+            dat = np.load(file)["jets"][: params["n_jets"], : params["n_const"]]
+            mask = dat.sum(-1) != 0
+        # Load data file
+        elif file.endswith("h5"):
+            if test:
+                stop_test=100000
+                #stop_test=params["n_jets"]
+                file = file.replace("val", "test")
+                file = file.replace("train", "test")
+                print(f"\nChanged file to {file}")
+                dat = pd.read_hdf(
+                    file,
+                    key=key,
+                    stop=params["n_jets_test"],
+                )
+            else:
+                dat = pd.read_hdf(file, key=key, stop=params["n_jets"])
+            dat = dat.to_numpy()[:, : params["n_const"] * 3]
+            dat = dat.reshape(dat.shape[0], -1, 3)
+            mask = dat[:, :, 0] != -1
+        else:
+            assert False, "Filetype for bg not supported"
+        # Delete empty jest (can occur in samplings)
+        mask = np.delete(mask, np.where(dat[:, 0, 0] == 0)[0], axis=0)
+        dat = np.delete(dat, np.where(dat[:, 0, 0] == 0)[0], axis=0)
+        dat[dat == -1] = 0
+        return dat, mask
+
+
+
+def load_data_val(params, test=False, plot_dists=None,):
+    def load_file(file, key=None):
+        file = file.replace("train", "val")
+        # Load  sample file
+        if file.endswith("npz"):
+            if test:
+                file = file.replace("train_", "test_")
+            print(f"\nChanged file to {file}")
+            dat = np.load(file)["jets"][: params["n_jets"], : params["n_const"]]
+            mask = dat.sum(-1) != 0
+        # Load data file
+        elif file.endswith("h5"):
+            if test:
+                stop_test=100000
+                #stop_test=params["n_jets"]
+                file = file.replace("val", "test")
+                file = file.replace("train", "test")
+                print(f"\nChanged file to {file}")
+                dat = pd.read_hdf(
+                    file,
+                    key=key,
+                    stop=params["n_jets_test"],
+                )
+            else:
+                dat = pd.read_hdf(file, key=key, stop=params["n_jets_val"])
+            dat = dat.to_numpy()[:, : params["n_const"] * 3]
+            dat = dat.reshape(dat.shape[0], -1, 3)
+            mask = dat[:, :, 0] != -1
+        else:
+            assert False, "Filetype for bg not supported"
+        # Delete empty jest (can occur in samplings)
+        mask = np.delete(mask, np.where(dat[:, 0, 0] == 0)[0], axis=0)
+        dat = np.delete(dat, np.where(dat[:, 0, 0] == 0)[0], axis=0)
+        dat[dat == -1] = 0
+        return dat, mask
+
+    print(params["bg_file"])
+    print(params["sig_file"])
+
+    bg, bg_mask = load_file(params["bg_file"], key=params["bg_key"])
+    sig, sig_mask = load_file(params["sig_file"], key=params["sig_key"])
+
+    if params["bg_key"] == "discretized":
+        print(f"BG made continuous, with noise {params['bg_noise']}\n")
+        bg = make_continues(bg,'bg', mask=bg_mask,bins_tag=params["bins_tag_bg"], noise=params["bg_noise"],)
+        
+    else:
+        bg[~bg_mask] = 0
+
+    if params["sig_key"] == "discretized":
+        print(f"Sig made continuous, with noise {params['bg_noise']}\n")
+        sig = make_continues(sig,'sig', mask=sig_mask,bins_tag=params["bins_tag_sig"] ,noise=params["sig_noise"])
+    else:
+        sig[~sig_mask] = 0
+
+    print(f"Bg shape {bg.shape}, Sig shape {sig.shape}\n")
+
+    bg = transform_momenta(bg, mask=bg_mask)
+    sig = transform_momenta(sig, mask=sig_mask)
+    data = np.append(bg, sig, 0)
+    labels = np.append(np.zeros(len(bg)), np.ones(len(sig)))
+    shuffle = np.random.permutation(len(data))
+
+    if not plot_dists is None:
+        import matplotlib.pyplot as plt
+
+        features = data.shape[-1]
+        fig, axes = plt.subplots(
+            features, 1, constrained_layout=True, figsize=(features * 3, 10)
+        )
+        for i in range(data.shape[-1]):
+            range_min = data[:, :, i].min()
+            range_max = data[:, :, i].max()
+            axes[i].hist(
+                bg[bg_mask, i],
+                bins=300,
+                range=[range_min, range_max],
+                histtype="step",
+                density=True,
+                label="Background",
+            )
+            axes[i].hist(
+                sig[sig_mask, i],
+                bins=300,
+                range=[range_min, range_max],
+                histtype="step",
+                density=True,
+                label="Signal",
+            )
+        axes[0].legend()
+        fig.savefig(plot_dists)
+        plt.close(fig)
+
+    return data[shuffle], labels[shuffle]
+
+
+if __name__ == "__main__":
+    pass
---
+++ particlenet/train_val.py
@@ -0,0 +1,146 @@
+import numpy as np
+from graphnet import GraphNet
+from data_loader_val import get_config, load_data
+import os
+import tensorflow as tf
+import time
+
+start = time.time()
+tfk = tf.keras
+
+#gpu = tf.config.list_physical_devices("GPU")
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+gpu = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpu[0], True)
+assert len(gpu) > 0, f"No GPU found, abort"
+
+def train_model(model, data, labels, train_params, logfolder, mask,data_val,mask_val,labels_val):
+    optimizer = tfk.optimizers.Adam(learning_rate=0.001)
+    model.compile(
+        optimizer,
+        loss="categorical_crossentropy",
+        metrics=[tfk.metrics.AUC(), "acc"],
+    )
+    
+    
+    if mask:
+        mask = data[:, :, 2] != 0
+        train_data = [data[:, :, :2], data, mask]
+    else:
+        train_data = [
+            data[:, :, :2],
+            data,
+        ]
+    
+    
+    if mask_val:
+        mask_val = data_val[:, :, 2] != 0
+        train_data_val = [data_val[:, :, :2], data_val, mask]
+    else:
+        train_data_val = [
+            data_val[:, :, :2],
+            data_val,
+        ]
+    
+
+    # Class weights
+    class_weight = {
+        0: len(labels) / np.sum(labels == 0),
+        1: len(labels) / np.sum(labels == 1),
+    }
+
+    tmp = min(class_weight.values())
+    class_weight = {key: class_weight[key] / tmp for key in class_weight}
+    print(f"Class weights: {class_weight}")
+
+    fit = model.fit(
+        train_data,
+        tfk.utils.to_categorical(labels, 2),
+        callbacks=[
+            tfk.callbacks.ReduceLROnPlateau(monitor="val_loss", patience=8, verbose=1),
+            tfk.callbacks.EarlyStopping(
+                monitor="val_loss", patience=12, verbose=1, restore_best_weights=True
+            ),
+            tfk.callbacks.ModelCheckpoint(
+                os.path.join(logfolder, "checkpoint_{epoch:02d}_.weights.h5"),
+                save_best_only=False,
+                save_freq=data.shape[0] // train_params["batch_size"] * 5,
+                save_weights_only=True,
+            ),
+        ],
+        class_weight=class_weight,
+        validation_data=(train_data_val,tfk.utils.to_categorical(labels_val, 2))
+        **train_params,
+    )
+    np.savez(os.path.join(logfolder, "training.npz"), **fit.history)
+
+
+def check_activation(config):
+    if config["graphnet"]["activation"] == "ReLU":
+        config["graphnet"]["activation"] = tfk.layers.ReLU()
+    elif config["graphnet"]["activation"] == "LeakyReLU":
+        config["graphnet"]["activation"] = tfk.layers.LeakyReLU(alpha=0.1)
+    else:
+        tmp = config["graphnet"]["activation"]
+        raise SystemExit(f"{tmp} activation not implemented")
+    return config
+
+
+def main():
+    config = check_activation(get_config())
+    model = GraphNet(**config["graphnet"])
+    start = time.time()
+    data, true_label = load_data(
+        config["data"],
+        plot_dists=os.path.join(config["logging"]["logfolder"], "distributions.png"),)
+    print(f"First labels {true_label[:15]}\n")
+
+
+    data_val, true_label_val = load_data_val(
+        config["data"],
+        plot_dists=os.path.join(config["logging"]["logfolder"], "distributions.png"),)
+    print(f"First labels {true_label[:15]}\n")
+    
+
+
+    train_model(
+        model=model,
+        data=data,
+        labels=true_label,
+        train_params=config["training"],
+        logfolder=config["logging"]["logfolder"],
+        mask=config["mask"],
+        data_val=data_val,
+        mask_val=config["mask"],
+        labels_val=true_label_val
+    )
+    model.save_weights(os.path.join(config["logging"]["logfolder"], "model_weights.h5"))
+    if config["mask"]:
+        mask = data[:, :, 2] != 0
+        test_data = [data[:, :, :2], data, mask]
+    else:
+        test_data = [data[:, :, :2], data]
+
+    predictions = model.predict(test_data, batch_size=1024, verbose=1)
+
+    np.savez(
+        os.path.join(config["logging"]["logfolder"], "predictions.npz"),
+        predictions=predictions,
+        region=true_label,
+        true_label=true_label,
+        seed=config["data"]["seed"],
+    )
+
+    checker = model.predict(
+        [
+            np.ones_like(data[:10]),
+            np.ones_like(data[:10]),
+            np.ones(data[:10].shape[:-1]),
+        ]
+    )
+    np.save(os.path.join(config["logging"]["logfolder"], "check.npy"), checker)
+    print(f"Script took {int(time.time() - start)} seconds")
+
+
+if __name__ == "__main__":
+    main()
