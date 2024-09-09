458838136a1fc733980723fd41d90797089bdcc0
HumbertosMac <you@example.com>
2024-09-09 15:55:06 +0200

    pnet landscape

--- particlenet/open_landscape.py
+++ particlenet/open_landscape.py
@@ -3,11 +3,20 @@ import pandas as pd
 import h5py
 
 # Open the HDF5 file in read mode
-file_path = '/home/home3/institut_thp/hreyes/Transformers/transformers_finke/datasets/TopQuarkTag/topquarktagging_data/discretized//val_qcd_all_bins403030.h5'
-with h5py.File(file_path, 'r') as h5_file:
-    # List all the datasets and groups in the file
-    print("Keys: %s" % h5_file.keys())
+file_path = '/home/home3/institut_thp/hreyes/Transformers/transformers_finke/datasets/TopQuarkTag/topquarktagging_data/discretized//train_qcd_all_bins403030.h5'
 
 
-dat = pd.read_hdf(file_path, key='raw', stop=1000)
+
+dat_train = pd.read_hdf(file_path, key='raw')
 print(dat)
+
+file_path_val = '/home/home3/institut_thp/hreyes/Transformers/transformers_finke/datasets/TopQuarkTag/topquarktagging_data/discretized//val_qcd_all_bins403030.h5'
+dat_val = pd.read_hdf(file_path, key='raw')
+
+
+
+file_path_all = '/home/home3/institut_thp/hreyes/Transformers/transformers_finke/datasets/TopQuarkTag/topquarktagging_data/discretized//all_qcd_all_bins403030.h5'
+data_all=pd.concat([dat_train,dat_val],axis=0)
+print(data_all.shape)
+
+data_all.to_csv(file_path_all,index=False)
