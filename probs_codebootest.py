75982e8520dc2b646e25af1bf9bdf8b9d2dcf8c8
HumbertosMac <you@example.com>
2024-10-23 15:45:22 +0200

    1const_codebook

--- probs_codebootest.py
+++ probs_codebootest.py
@@ -7,7 +7,7 @@ model_path='/net/data_ttk/hreyes/JetClass/OptClass/TTBar_models/TTBar_run_test__
 model_type='model_best.pt'
 num_const=1
 tag_foreval='test_eval_codebook_nconst1_endtokentrue'
-
+num_events=39401
 
 command_eval='python evaluate_probabilities.py --model '+str(model_path)+str(model_type)+' --data '+str(test_dataset)+' --tag '+tag_foreval+' --num_const '+str(num_const)+' --num_events '+str(num_samples)+' --fixed_samples'
 os.system(command_eval)
